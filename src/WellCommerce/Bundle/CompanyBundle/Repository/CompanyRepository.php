bd3388a70ae0d220d66b347049e72e9ae8424e65
Adam Piotrowski <adam@gekosale.com>
2014-08-11 16:52:59 +0200

    Changed company repository

---
+++ src/WellCommerce/Bundle/CompanyBundle/Repository/CompanyRepository.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\CompanyBundle\Repository;
+
+use WellCommerce\Bundle\CoreBundle\Repository\AbstractEntityRepository;
+
+/**
+ * Class CompanyRepository
+ *
+ * @package WellCommerce\Bundle\CompanyBundle\Repository
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class CompanyRepository extends AbstractEntityRepository implements CompanyRepositoryInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getDataGridQueryBuilder()
+    {
+        return parent::getQueryBuilder()->groupBy('company.id');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function updateRow(array $request)
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function deleteMultipleRows(array $ids)
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAlias()
+    {
+        return 'company';
+    }
+}
