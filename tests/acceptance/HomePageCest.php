3a84cbad8b004a525dda8a7c43a48c909ed21b10
Adam Piotrowski <adam@gekosale.com>
2014-08-13 00:05:13 +0200

    Cest

---
+++ tests/acceptance/HomePageCest.php
@@ -0,0 +1,20 @@
+<?php
+
+class HomePageCest
+{
+    public function _before()
+    {
+    }
+
+    public function _after()
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+        $I->wantTo('Check if home page triggers error');
+        $I->amOnPage('/');
+        $I->see('No route found for');
+    }
+}
