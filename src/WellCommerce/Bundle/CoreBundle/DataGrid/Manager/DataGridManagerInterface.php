f1d30943b641d8b1edaa0b29c15f998f03b94051
Adam Piotrowski <adam@gekosale.com>
2014-08-12 09:38:48 +0200

    DataGridManager

---
+++ src/WellCommerce/Bundle/CoreBundle/DataGrid/Manager/DataGridManagerInterface.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\CoreBundle\DataGrid\Manager;
+
+/**
+ * Interface DataGridManagerInterface
+ *
+ * @package WellCommerce\Bundle\CoreBundle\DataGrid\Manager
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+interface DataGridManagerInterface {
+
+} 
