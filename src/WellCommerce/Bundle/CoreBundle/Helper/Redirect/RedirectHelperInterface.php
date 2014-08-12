7b377def82844361170738d400ed5074ba39cc1b
Adam Piotrowski <adam@gekosale.com>
2014-08-12 09:38:37 +0200

    RedirectHelperInterface to use own implementations

---
+++ src/WellCommerce/Bundle/CoreBundle/Helper/Redirect/RedirectHelperInterface.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\CoreBundle\Helper\Redirect;
+
+/**
+ * Interface RedirectHelperInterface
+ *
+ * @package WellCommerce\Bundle\CoreBundle\Helper\Redirect
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+interface RedirectHelperInterface {
+
+    /**
+     * Redirects user to another resource
+     *
+     * @param       $route
+     * @param array $routeParams
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function redirectTo($route, array $routeParams = []);
+
+    /**
+     * Resolves current route and redirects user to given controller action
+     *
+     * @param       $action
+     * @param array $params
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function redirectToAction($action, array $params = []);
+} 
