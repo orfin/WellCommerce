e4ec28946dc1fcd3ed7f8cbe0cd0614d9def178d
Adam Piotrowski <adam@gekosale.com>
2014-08-16 11:53:27 +0200

    Migration

---
+++ app/sql/migrations/Version20140816095231.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace WellCommerce\Migration;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20140816095231 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("ALTER TABLE shop DROP INDEX UNIQ_AC6A4CA2979B1AD6, ADD INDEX IDX_AC6A4CA2979B1AD6 (company_id)");
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("ALTER TABLE shop DROP INDEX IDX_AC6A4CA2979B1AD6, ADD UNIQUE INDEX UNIQ_AC6A4CA2979B1AD6 (company_id)");
+    }
+}
