17bccb30f8e2f680efdcf457e719c18d6610666f
Adam Piotrowski <adam@gekosale.com>
2014-08-17 20:12:53 +0200

    After Vagrant

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,4 +1,14 @@
 <?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
 
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
