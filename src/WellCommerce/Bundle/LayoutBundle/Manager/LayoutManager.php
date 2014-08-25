e12bd8c0ed2dd2c04af455ac07a7ef559ceb7eea
Adam Piotrowski <adam@gekosale.com>
2014-08-25 22:58:11 +0200

    LayoutManager changes 2

---
+++ src/WellCommerce/Bundle/LayoutBundle/Manager/LayoutManager.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+namespace WellCommerce\Bundle\LayoutBundle\Manager;
+
+use WellCommerce\Bundle\CoreBundle\DependencyInjection\AbstractContainer;
+use WellCommerce\Bundle\LayoutBundle\Manager\Box\LayoutBoxConfiguratorInterface;
+
+/**
+ * Class LayoutManager
+ *
+ * @package WellCommerce\Bundle\LayoutBundle\Manager
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class LayoutManager extends AbstractContainer
+{
+    /**
+     * @var array Array containing all layout box configurators
+     */
+    private $layoutBoxConfigurators = [];
+
+    /**
+     * Adds new layout box configurator to stack
+     *
+     * @param                                $id
+     * @param LayoutBoxConfiguratorInterface $configurator
+     */
+    public function addLayoutBoxConfigurator($alias, LayoutBoxConfiguratorInterface $configurator)
+    {
+        if (isset($this->layoutBoxConfigurators[$alias])) {
+            throw new \LogicException(sprintf('LayoutBoxConfigurator "%s" is already registered. Cannot register twice the same configurator. Please change its alias.'), $alias);
+        }
+        $this->layoutBoxConfigurators[$alias] = $configurator;
+    }
+
+    /**
+     * Returns a configurator by its alias
+     *
+     * @param $alias
+     *
+     * @return mixed
+     * @throws \RuntimeException
+     */
+    public function getLayoutBoxConfigurator($alias)
+    {
+        if (!isset($this->layoutBoxConfigurators[$alias])) {
+            throw new \RuntimeException(sprintf('LayoutBoxConfigurator "%s" is not registered', $alias));
+        }
+
+        return $this->layoutBoxConfigurators[$alias];
+    }
+
+    /**
+     * Returns all layout box configurators
+     *
+     * @return array
+     */
+    public function getLayoutBoxConfigurators()
+    {
+        return $this->layoutBoxConfigurators;
+    }
+}
