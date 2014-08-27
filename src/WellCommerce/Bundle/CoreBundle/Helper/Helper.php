9148284f7ba938c210260b859412a76fd608404f
Adam Piotrowski <adam@gekosale.com>
2014-08-27 23:19:29 +0200

    Removed old helpers

--- src/WellCommerce/Bundle/CoreBundle/Helper/Helper.php
+++ src/WellCommerce/Bundle/CoreBundle/Helper/Helper.php
@@ -34,24 +34,6 @@ class Helper extends AbstractContainer
     }
 
     /**
-     * Makes a sluggable from passed string
-     *
-     * @param        $string
-     * @param string $delimiter
-     *
-     * @return string
-     */
-    public static function makeSlug($string, $delimiter = '-')
-    {
-        $clean = iconv('UTF-8', 'ASCII//TRANSLIT', $string);
-        $clean = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $clean);
-        $clean = strtolower(trim($clean, '-'));
-        $clean = preg_replace("/[\/_|+ -]+/", $delimiter, $clean);
-
-        return $clean;
-    }
-
-    /**
      * Converts string to snake-case
      *
      * @param string $value
