bd80bae65cab36df6a330ac3f851061362c3a909
Adam Piotrowski <adam@gekosale.com>
2014-09-08 21:57:16 +0200

    Forms refactoring 2

---
+++ src/WellCommerce/Bundle/CoreBundle/Form/Elements/AbstractContainer.php
@@ -0,0 +1,130 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\CoreBundle\Form\Elements;
+
+/**
+ * Class AbstractContainer
+ *
+ * @package WellCommerce\Bundle\CoreBundle\Form\Elements
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+abstract class AbstractContainer extends AbstractNode
+{
+    protected $children = [];
+    protected $tabsOffset = '';
+
+    public function addRule($type, $options = [])
+    {
+        foreach ($this->children as $child) {
+            $child->addRule($type, $options);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addFilter($type, $options = [])
+    {
+        foreach ($this->children as $child) {
+            $child->addFilter($type, $options);
+        }
+    }
+
+    public function setDefaults($values)
+    {
+        foreach ($this->children as $child) {
+            $child->setDefaults($values);
+        }
+    }
+
+    public function populate($value)
+    {
+        if (isset($value) && is_array($value) && $this->isIterated($value)) {
+            foreach ($this->children as $child) {
+                $valueArray = [];
+                if (isset($value) && is_array($value)) {
+                    foreach ($value as $i => $repetition) {
+                        $name = $child->getName();
+                        if (!empty($name)) {
+                            if (isset($repetition[$name])) {
+                                $valueArray[$i] = $repetition[$name];
+                            } else {
+                                $valueArray[$i] = '';
+                            }
+                        }
+                    }
+                }
+                $child->populate($valueArray);
+            }
+        } else {
+            // simple value
+            foreach ($this->children as $child) {
+                $name = $child->getName();
+                if (empty($name)) {
+                    continue;
+                }
+                if (isset($value[$name])) {
+                    $child->populate($value[$name]);
+                } elseif ($this->form->_populatingWholeForm) {
+                    $child->populate(null);
+                }
+            }
+        }
+    }
+
+    public function getChildren()
+    {
+        return $this->children;
+    }
+
+    public function getFields()
+    {
+        return $this->form->fields;
+    }
+
+    public function renderChildren()
+    {
+        $render = [];
+        foreach ($this->children as $child) {
+            $render[] = $child->render($this->renderMode, $this->tabs . $this->tabsOffset);
+        }
+
+        return implode(',', $render);
+    }
+
+    public function isValid($values = [])
+    {
+        $result = true;
+        foreach ($this->children as $child) {
+            if (!$child->isValid()) {
+                $result = false;
+            }
+        }
+
+        return $result;
+    }
+
+    protected function getValues()
+    {
+        $values = [];
+        foreach ($this->children as $child) {
+            if ($child instanceof Container) {
+                $values[$child->getName()] = $child->getValues();
+            } elseif ($child instanceof Field) {
+                $values[$child->getName()] = $child->getValue();
+            }
+        }
+
+        return $values;
+    }
+}
