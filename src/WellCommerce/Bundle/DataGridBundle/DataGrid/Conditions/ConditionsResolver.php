ace01efa4b0654ef8a782d5102a2ce9882f5a0ff
Adam Piotrowski <adam@gekosale.com>
2014-12-16 01:05:31 +0100

    DataSet

---
+++ src/WellCommerce/Bundle/DataGridBundle/DataGrid/Conditions/ConditionsResolver.php
@@ -0,0 +1,92 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\DataGridBundle\DataGrid\Conditions;
+
+use WellCommerce\Bundle\DataSetBundle\DataSet\Column\ColumnCollection;
+use WellCommerce\Bundle\DataSetBundle\DataSet\Conditions\Condition\Eq;
+use WellCommerce\Bundle\DataSetBundle\DataSet\Conditions\Condition\Gte;
+use WellCommerce\Bundle\DataSetBundle\DataSet\Conditions\Condition\In;
+use WellCommerce\Bundle\DataSetBundle\DataSet\Conditions\Condition\Like;
+use WellCommerce\Bundle\DataSetBundle\DataSet\Conditions\Condition\Lte;
+use WellCommerce\Bundle\DataSetBundle\DataSet\Conditions\Condition\Neq;
+use WellCommerce\Bundle\DataSetBundle\DataSet\Conditions\ConditionsCollection;
+
+/**
+ * Class ConditionsResolver
+ *
+ * @package WellCommerce\Bundle\DataGridBundle\DataGrid\Conditions
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class ConditionsResolver
+{
+    /**
+     * @var ColumnCollection
+     */
+    protected $columns;
+
+    /**
+     * Constructor
+     *
+     * @param ColumnCollection $collection
+     */
+    public function __construct(ColumnCollection $collection)
+    {
+        $this->columns = $collection;
+    }
+
+    /**
+     * Resolves "where" params and creates conditions collection
+     *
+     * @param array $params
+     *
+     * @return null|ConditionsCollection
+     */
+    public function resolve($params)
+    {
+        if (is_array($params)) {
+            $conditions = new ConditionsCollection();
+
+            foreach ($params as $where) {
+                $column     = $where['column'];
+                $operator   = $where['operator'];
+                $value      = isset($where['value']) ? $where['value'] : null;
+                $expression = null;
+
+                switch ($operator) {
+                    case 'IN':
+                        $conditions->add(new In($column, $value));
+                        break;
+                    case 'NE':
+                        $conditions->add(new Neq($column, $value));
+                        break;
+                    case 'LE':
+                        $conditions->add(new Lte($column, $value));
+                        break;
+                    case 'GE':
+                        $conditions->add(new Gte($column, $value));
+                        break;
+                    case 'LIKE':
+                        $conditions->add(new Like($column, $value));
+                        break;
+                    default:
+                        $conditions->add(new Eq($column, $value));
+                        break;
+                }
+            }
+
+            return $conditions;
+        }
+
+        return null;
+    }
+}
---
+++ src/WellCommerce/Bundle/DataSetBundle/DataSet/Conditions/Condition/Like.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\DataSetBundle\DataSet\Conditions\Condition;
+
+use WellCommerce\Bundle\DataSetBundle\DataSet\Conditions\AbstractCondition;
+use WellCommerce\Bundle\DataSetBundle\DataSet\Conditions\ConditionInterface;
+
+/**
+ * Class Like
+ *
+ * @package WellCommerce\Bundle\DataSetBundle\DataSet\Conditions\Condition
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class Like extends AbstractCondition implements ConditionInterface
+{
+    protected $operator = 'like';
+} 
