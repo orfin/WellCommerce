4c73872be598ce7b9ed707ffec968b2ad5f7e269
Adam Piotrowski <adam@gekosale.com>
2014-12-20 02:17:37 +0100

    The method Symfony\Component\EventD...\Event::getDispatcher() has been deprecated with message: Deprecated in 2.4, to be removed in 3.0. The

--- src/WellCommerce/Bundle/AdminBundle/EventListener/AdminSubscriber.php
+++ src/WellCommerce/Bundle/AdminBundle/EventListener/AdminSubscriber.php
@@ -13,6 +13,7 @@ namespace WellCommerce\Bundle\AdminBundle\EventListener;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\EventDispatcher\Event;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RequestMatcher;
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
@@ -32,8 +33,6 @@ use WellCommerce\Bundle\CoreBundle\EventListener\AbstractEventSubscriber;
  */
 class AdminSubscriber extends AbstractEventSubscriber
 {
-    const LOGIN_ROUTE = 'admin.user.login';
-
     public static function getSubscribedEvents()
     {
         return [
@@ -41,7 +40,7 @@ class AdminSubscriber extends AbstractEventSubscriber
         ];
     }
 
-    public function onKernelController(FilterControllerEvent $event)
+    public function onKernelController(FilterControllerEvent $event, $eventName, EventDispatcherInterface $dispatcher)
     {
         // admin menu will be rendered only when HttpKernelInterface::MASTER_REQUEST
         if ($event->getRequestType() == HttpKernelInterface::SUB_REQUEST) {
@@ -58,7 +57,7 @@ class AdminSubscriber extends AbstractEventSubscriber
             // propagates main event to other event subscribers to build the menu
             $menuEvent = new AdminMenuEvent($builder);
 
-            $event->getDispatcher()->dispatch(AdminMenuEvent::INIT_EVENT, $menuEvent);
+            $dispatcher->dispatch(AdminMenuEvent::INIT_EVENT, $menuEvent);
 
             // saves menu data in session
             $this->container->get('session')->set('admin/menu', $menuEvent->getBuilder()->getMenu());
