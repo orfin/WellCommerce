eef0dbaf95b221300da0c234f357b2cda1b2b6d8
Adam Piotrowski <adam@gekosale.com>
2015-01-03 15:05:03 +0100

    Tree fixes

--- src/WellCommerce/Bundle/CoreBundle/Form/Elements/Tree/SortableList.php
+++ src/WellCommerce/Bundle/CoreBundle/Form/Elements/Tree/SortableList.php
@@ -12,7 +12,6 @@
 
 namespace WellCommerce\Bundle\CoreBundle\Form\Elements\Tree;
 
-use WellCommerce\Bundle\CoreBundle\Form\Elements\AbstractField;
 use WellCommerce\Bundle\CoreBundle\Form\Elements\ElementInterface;
 
 /**
@@ -20,32 +19,7 @@ use WellCommerce\Bundle\CoreBundle\Form\Elements\ElementInterface;
  *
  * @author  Adam Piotrowski <adam@wellcommerce.org>
  */
-class SortableList extends AbstractField implements ElementInterface
+class SortableList extends AbstractTree implements ElementInterface
 {
-    public function prepareAttributesJs()
-    {
-        return [
-            $this->formatAttributeJs('name', 'sName'),
-            $this->formatAttributeJs('label', 'sLabel'),
-            $this->formatAttributeJs('error', 'sError'),
-            $this->formatAttributeJs('clickable', 'bClickable'),
-            $this->formatAttributeJs('deletable', 'bDeletable'),
-            $this->formatAttributeJs('sortable', 'bSortable'),
-            $this->formatAttributeJs('addable', 'bAddable'),
-            $this->formatAttributeJs('items', 'oItems', ElementInterface::TYPE_OBJECT),
-            $this->formatAttributeJs('total', 'iTotal'),
-            $this->formatAttributeJs('onClick', 'fOnClick', ElementInterface::TYPE_FUNCTION),
-            $this->formatAttributeJs('onAdd', 'fOnAdd', ElementInterface::TYPE_FUNCTION),
-            $this->formatAttributeJs('onAfterAdd', 'fOnAfterAdd', ElementInterface::TYPE_FUNCTION),
-            $this->formatAttributeJs('onDelete', 'fOnDelete', ElementInterface::TYPE_FUNCTION),
-            $this->formatAttributeJs('onAfterDelete', 'fOnAfterDelete', ElementInterface::TYPE_FUNCTION),
-            $this->formatAttributeJs('onSaveOrder', 'fOnSaveOrder', ElementInterface::TYPE_FUNCTION),
-            $this->formatAttributeJs('active', 'sActive'),
-            $this->formatAttributeJs('add_item_prompt', 'sAddItemPrompt'),
-            $this->formatAttributeJs('delete_item_prompt', 'sDeleteItemPrompt'),
-            $this->formatAttributeJs('set', 'sSet'),
-            $this->formatRulesJs(),
-            $this->formatDependencyJs(),
-        ];
-    }
+
 }
--- src/WellCommerce/Bundle/CoreBundle/Form/Elements/Tree/Tree.php
+++ src/WellCommerce/Bundle/CoreBundle/Form/Elements/Tree/Tree.php
@@ -22,7 +22,7 @@ use WellCommerce\Bundle\CoreBundle\Form\Elements\ElementInterface;
  *
  * @author  Adam Piotrowski <adam@wellcommerce.org>
  */
-class Tree extends AbstractField implements ElementInterface
+class Tree extends AbstractTree implements ElementInterface
 {
     /**
      * {@inheritdoc}
@@ -97,42 +97,5 @@ class Tree extends AbstractField implements ElementInterface
             'clickable_root'                   => 'bool',
             'set'                              => 'string',
         ]);
-    }
-
-    public function prepareAttributesJs()
-    {
-        return [
-            $this->formatAttributeJs('name', 'sName'),
-            $this->formatAttributeJs('label', 'sLabel'),
-            $this->formatAttributeJs('addLabel', 'sAddLabel'),
-            $this->formatAttributeJs('error', 'sError'),
-            $this->formatAttributeJs('selectable', 'bSelectable'),
-            $this->formatAttributeJs('choosable', 'bChoosable'),
-            $this->formatAttributeJs('clickable', 'bClickable'),
-            $this->formatAttributeJs('deletable', 'bDeletable'),
-            $this->formatAttributeJs('sortable', 'bSortable'),
-            $this->formatAttributeJs('retractable', 'bRetractable'),
-            $this->formatAttributeJs('addable', 'bAddable'),
-            $this->formatAttributeJs('total', 'iTotal'),
-            $this->formatAttributeJs('restrict', 'iRestrict'),
-            $this->formatAttributeJs('items', 'oItems', ElementInterface::TYPE_OBJECT),
-            $this->formatAttributeJs('onClick', 'fOnClick', ElementInterface::TYPE_FUNCTION),
-            $this->formatAttributeJs('onDuplicate', 'fOnDuplicate', ElementInterface::TYPE_FUNCTION),
-            $this->formatAttributeJs('onAdd', 'fOnAdd', ElementInterface::TYPE_FUNCTION),
-            $this->formatAttributeJs('onAfterAdd', 'fOnAfterAdd', ElementInterface::TYPE_FUNCTION),
-            $this->formatAttributeJs('onDelete', 'fOnDelete', ElementInterface::TYPE_FUNCTION),
-            $this->formatAttributeJs('onAfterDelete', 'fOnAfterDelete', ElementInterface::TYPE_FUNCTION),
-            $this->formatAttributeJs('onSaveOrder', 'fOnSaveOrder', ElementInterface::TYPE_FUNCTION),
-            $this->formatAttributeJs('active', 'sActive'),
-            $this->formatAttributeJs('onAfterDeleteId', 'sOnAfterDeleteId'),
-            $this->formatAttributeJs('add_item_prompt', 'sAddItemPrompt'),
-            $this->formatAttributeJs('getchildren', 'fGetChildren', ElementInterface::TYPE_FUNCTION),
-            $this->formatAttributeJs('prevent_duplicates', 'bPreventDuplicates', ElementInterface::TYPE_BOOLEAN),
-            $this->formatAttributeJs('prevent_duplicates_on_all_levels', 'bPreventDuplicatesOnAllLevels',
-                ElementInterface::TYPE_BOOLEAN),
-            $this->formatAttributeJs('set', 'sSet'),
-            $this->formatRulesJs(),
-            $this->formatDependencyJs(),
-        ];
     }
 }
