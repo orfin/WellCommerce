ccc6f726394427e94a44340b22735000aca1f763
Adam Piotrowski <adam@gekosale.com>
2015-01-07 21:25:37 +0100

    Fixed product photos

---
+++ src/WellCommerce/Bundle/MediaBundle/Form/MediaFormBuilder.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+namespace WellCommerce\Bundle\MediaBundle\Form;
+
+use WellCommerce\Bundle\CoreBundle\Form\Builder\AbstractFormBuilder;
+use WellCommerce\Bundle\CoreBundle\Form\Builder\FormBuilderInterface;
+use WellCommerce\Bundle\CoreBundle\Form\Elements\FormInterface;
+
+/**
+ * Class MediaForm
+ *
+ * @package WellCommerce\Media\Form
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class MediaFormBuilder extends AbstractFormBuilder implements FormBuilderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormInterface $form)
+    {
+        $requiredData = $form->addChild($this->getElement('fieldset', [
+            'name'  => 'required_data',
+            'label' => $this->trans('form.required_data.label')
+        ]));
+
+        $requiredData->addChild($this->getElement('image', [
+            'name'     => 'required_data',
+            'label'    => $this->trans('form.required_data.label'),
+            'datagrid' => $this->get('media.datagrid')
+        ]));
+
+        $form->addFilter($this->getFilter('no_code'));
+        $form->addFilter($this->getFilter('trim'));
+        $form->addFilter($this->getFilter('secure'));
+    }
+}
