d5145453b2af5d1e34255f839f0ee41cd57f31b7
Adam Piotrowski <adam@gekosale.com>
2015-01-09 00:22:49 +0100

    Fixed client group form

---
+++ src/WellCommerce/Bundle/ClientBundle/Form/ClientGroupFormBuilder.php
@@ -0,0 +1,66 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+namespace WellCommerce\Bundle\ClientBundle\Form;
+
+use WellCommerce\Bundle\CoreBundle\Form\Builder\AbstractFormBuilder;
+use WellCommerce\Bundle\CoreBundle\Form\Builder\FormBuilderInterface;
+use WellCommerce\Bundle\CoreBundle\Form\DataTransformer\TranslationTransformer;
+use WellCommerce\Bundle\CoreBundle\Form\Elements\FormInterface;
+
+/**
+ * Class ClientGroupForm
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class ClientGroupFormBuilder extends AbstractFormBuilder implements FormBuilderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormInterface $form)
+    {
+        $requiredData = $form->addChild($this->getElement('nested_fieldset', [
+            'name'  => 'required_data',
+            'label' => $this->trans('form.required_data.label')
+        ]));
+
+        $requiredData->addChild($this->getElement('text_field', [
+            'name'    => 'discount',
+            'label'   => $this->trans('client_group.discount.label'),
+            'comment' => $this->trans('client_group.discount.comment'),
+            'suffix'  => '%',
+            'filters' => [
+                $this->getFilter('comma_to_dot_changer')
+            ],
+        ]));
+
+        $languageData = $requiredData->addChild($this->getElement('language_fieldset', [
+            'name'  => 'translations',
+            'label' => $this->trans('form.required_data.language_data.label'),
+            'transformer' => new TranslationTransformer($this->get('client_group.repository'))
+        ]));
+
+        $languageData->addChild($this->getElement('text_field', [
+            'name'  => 'name',
+            'label' => $this->trans('client_group.name.label'),
+            'rules' => [
+                $this->getRule('required', [
+                    'message' => $this->trans('client_group.name.error.required')
+                ]),
+            ]
+        ]));
+
+        $form->addFilter($this->getFilter('no_code'));
+        $form->addFilter($this->getFilter('trim'));
+        $form->addFilter($this->getFilter('secure'));
+    }
+}
