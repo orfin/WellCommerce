bd07ba01ac0880abd99a0aa399728b4d3f0d4c02
Adam Piotrowski <adam@gekosale.com>
2015-01-09 00:26:57 +0100

    Fixed company

--- src/WellCommerce/Bundle/CompanyBundle/Controller/Admin/CompanyController.php
+++ src/WellCommerce/Bundle/CompanyBundle/Controller/Admin/CompanyController.php
@@ -12,25 +12,15 @@
 
 namespace WellCommerce\Bundle\CompanyBundle\Controller\Admin;
 
-use Symfony\Component\HttpFoundation\Request;
 use WellCommerce\Bundle\CoreBundle\Controller\Admin\AbstractAdminController;
 
 /**
  * Class CompanyController
  *
- * @package WellCommerce\Bundle\CompanyBundle\Controller
  * @author  Adam Piotrowski <adam@wellcommerce.org>
  *
  * @Sensio\Bundle\FrameworkExtraBundle\Configuration\Template()
  */
 class CompanyController extends AbstractAdminController
 {
-    public function indexAction(Request $request)
-    {
-        $datagrid = $this->get('company.datagrid');
-
-        return [
-            'datagrid' => $datagrid->getInstance()
-        ];
-    }
 }
