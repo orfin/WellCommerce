50a7140b0e033d7a7d34d57f252e622f58fe7cc9
Adam Piotrowski <adam@gekosale.com>
2015-01-08 15:39:13 +0100

    Forms

---
+++ src/WellCommerce/Bundle/IntlBundle/Form/CurrencyFormBuilder.php
@@ -0,0 +1,63 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+namespace WellCommerce\Bundle\IntlBundle\Form;
+
+use WellCommerce\Bundle\CoreBundle\Form\Builder\AbstractFormBuilder;
+use WellCommerce\Bundle\CoreBundle\Form\Builder\FormBuilderInterface;
+use WellCommerce\Bundle\CoreBundle\Form\Elements\FormInterface;
+use WellCommerce\Bundle\IntlBundle\Repository\CurrencyRepositoryInterface;
+
+/**
+ * Class CurrencyFormBuilder
+ *
+ * @author Adam Piotrowski <adam@wellcommerce.org>
+ */
+class CurrencyFormBuilder extends AbstractFormBuilder implements FormBuilderInterface
+{
+    /**
+     * @var CurrencyRepositoryInterface
+     */
+    private $repository;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormInterface $form)
+    {
+        $requiredData = $form->addChild($this->getElement('nested_fieldset', [
+            'name'  => 'required_data',
+            'label' => $this->trans('form.required_data')
+        ]));
+
+        $requiredData->addChild($this->getElement('select', [
+            'name'    => 'code',
+            'label'   => $this->trans('currency.code'),
+            'options' => $this->repository->getCurrenciesToSelect()
+        ]));
+
+        $form->addFilter($this->getFilter('no_code'));
+        $form->addFilter($this->getFilter('trim'));
+        $form->addFilter($this->getFilter('secure'));
+
+        return $form;
+    }
+
+    /**
+     * Sets currency repository
+     *
+     * @param CurrencyRepositoryInterface $repository
+     */
+    public function setRepository(CurrencyRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+}
---
+++ src/WellCommerce/Bundle/LayoutBundle/Form/LayoutBoxFormBuilder.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+namespace WellCommerce\Bundle\LayoutBundle\Form;
+
+use Symfony\Component\Finder\Finder;
+use WellCommerce\Bundle\CoreBundle\Form\Builder\AbstractFormBuilder;
+use WellCommerce\Bundle\CoreBundle\Form\Builder\FormBuilderInterface;
+use WellCommerce\Bundle\CoreBundle\Form\DataTransformer\TranslationTransformer;
+use WellCommerce\Bundle\CoreBundle\Form\Elements\FormInterface;
+
+/**
+ * Class LayoutBoxFormBuilder
+ *
+ * @author Adam Piotrowski <adam@wellcommerce.org>
+ */
+class LayoutBoxFormBuilder extends AbstractFormBuilder implements FormBuilderInterface
+{
+    const FORM_INIT_EVENT = 'layout_box.form.init';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormInterface $form)
+    {
+        $requiredData = $form->addChild($this->getElement('nested_fieldset', [
+            'name'  => 'required_data',
+            'label' => $this->trans('Required data')
+        ]));
+
+        $languageData = $requiredData->addChild($this->getElement('language_fieldset', [
+            'name'        => 'translations',
+            'label'       => $this->trans('form.required_data.language_data.label'),
+            'transformer' => new TranslationTransformer($this->get('layout_box.repository'))
+        ]));
+
+        $languageData->addChild($this->getElement('text_field', [
+            'name'  => 'name',
+            'label' => $this->trans('Name'),
+            'rules' => [
+                $this->getRule('required', [
+                    'message' => $this->trans('layout_box.name.required')
+                ]),
+            ]
+        ]));
+
+        $requiredData->addChild($this->getElement('text_field', [
+            'name'    => 'identifier',
+            'label'   => $this->trans('layout_box.identifier.label'),
+            'comment' => $this->trans('layout_box.identifier.comment'),
+        ]));
+
+        $requiredData->addChild($this->getElement('select', [
+            'name'  => 'boxType',
+            'label' => $this->trans('layout_box.box_type.label'),
+        ]));
+
+        $form->addFilter($this->getFilter('no_code'));
+        $form->addFilter($this->getFilter('trim'));
+        $form->addFilter($this->getFilter('secure'));
+    }
+}
