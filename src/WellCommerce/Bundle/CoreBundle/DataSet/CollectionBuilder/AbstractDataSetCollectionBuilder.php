bb567e6e3694a8124f38f1d2758e0e7e5739ccfd
Adam Piotrowski <adam@gekosale.com>
2015-01-10 00:15:44 +0100

    Removed collection qb in favor of datasets

---
+++ src/WellCommerce/Bundle/CoreBundle/DataSet/CollectionBuilder/AbstractDataSetCollectionBuilder.php
@@ -0,0 +1,109 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\CoreBundle\DataSet\CollectionBuilder;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use WellCommerce\Bundle\CoreBundle\DataSet\DataSetInterface;
+use WellCommerce\Bundle\CoreBundle\DataSet\Request\DataSetRequest;
+
+/**
+ * Class AbstractDataSetCollectionBuilder
+ *
+ * @author Adam Piotrowski <adam@wellcommerce.org>
+ */
+abstract class AbstractDataSetCollectionBuilder
+{
+    /**
+     * @var DataSetInterface
+     */
+    protected $dataset;
+
+    /**
+     * @var array
+     */
+    protected $options;
+
+    /**
+     * @var \Symfony\Component\PropertyAccess\PropertyAccessor
+     */
+    protected $propertyAccessor;
+
+    /**
+     * Constructor
+     *
+     * @param DataSetInterface $dataset
+     * @param array            $options
+     */
+    public function __construct(DataSetInterface $dataset, array $options = [])
+    {
+        $this->dataset   = $dataset;
+        $optionsResolver = new OptionsResolver();
+        $this->configureOptions($optionsResolver);
+        $this->options          = $optionsResolver->resolve($options);
+        $this->propertyAccessor = PropertyAccess::createPropertyAccessor();
+    }
+
+    /**
+     * Configures collection builder options
+     *
+     * @param OptionsResolver $resolver
+     */
+    protected function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setRequired([
+            'limit',
+            'order_by',
+            'order_dir',
+        ]);
+
+        $resolver->setDefaults([
+            'conditions' => null
+        ]);
+
+        $resolver->setAllowedTypes([
+            'limit'      => ['numeric'],
+            'order_by'   => ['string'],
+            'order_dir'  => ['string'],
+            'conditions' => ['null', 'WellCommerce\Bundle\CoreBundle\DataSet\Conditions\ConditionsCollection']
+        ]);
+    }
+
+    /**
+     * Creates new dataset request object
+     *
+     * @return DataSetRequest
+     */
+    protected function getDataSetRequest()
+    {
+        return new DataSetRequest([
+            'limit'      => $this->options['limit'],
+            'orderBy'    => $this->options['order_by'],
+            'orderDir'   => $this->options['order_dir'],
+            'conditions' => $this->options['conditions'],
+        ]);
+    }
+
+    /**
+     * Returns dataset rows
+     *
+     * @return array
+     */
+    protected function getDataSetRows()
+    {
+        $request = $this->getDataSetRequest();
+        $results = $this->dataset->getResults($request);
+
+        return $results['rows'];
+    }
+}
---
+++ src/WellCommerce/Bundle/TaxBundle/Form/TaxFormBuilder.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+namespace WellCommerce\Bundle\TaxBundle\Form;
+
+use WellCommerce\Bundle\CoreBundle\Entity\BaseSubjectInterface;
+use WellCommerce\Bundle\CoreBundle\Form\Builder\AbstractFormBuilder;
+use WellCommerce\Bundle\CoreBundle\Form\Builder\FormBuilderInterface;
+use WellCommerce\Bundle\CoreBundle\Form\Elements\FormInterface;
+
+/**
+ * Class TaxFormBuilder
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class TaxFormBuilder extends AbstractFormBuilder implements FormBuilderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormInterface $form)
+    {
+        $requiredData = $form->addChild($this->getElement('nested_fieldset', [
+            'name'  => 'required_data',
+            'label' => $this->trans('form.required_data.label')
+        ]));
+
+        $requiredData->addChild($this->getElement('text_field', [
+            'name'    => 'value',
+            'label'   => $this->trans('tax.required_data.value.label'),
+        ]));
+
+        $languageData = $requiredData->addChild($this->getElement('language_fieldset', [
+            'name'  => 'translations',
+            'label' => $this->trans('form.required_data.language_data.label')
+        ]));
+
+        $languageData->addChild($this->getElement('text_field', [
+            'name'  => 'name',
+            'label' => $this->trans('tax.language_data.name.label'),
+        ]));
+
+        $form->addFilter($this->getFilter('no_code'));
+        $form->addFilter($this->getFilter('trim'));
+        $form->addFilter($this->getFilter('secure'));
+    }
+}
