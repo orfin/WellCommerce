357e96edee804cd6571c897967210af45b89bc0d
Adam Piotrowski <adam@gekosale.com>
2015-01-15 11:19:29 +0100

    Fixed PaymentBundle

--- src/WellCommerce/Bundle/PaymentBundle/DataGrid/PaymentMethodDataGrid.php
+++ src/WellCommerce/Bundle/PaymentBundle/DataGrid/PaymentMethodDataGrid.php
@@ -15,6 +15,9 @@ use WellCommerce\Bundle\CoreBundle\DataGrid\AbstractDataGrid;
 use WellCommerce\Bundle\CoreBundle\DataGrid\Column\Column;
 use WellCommerce\Bundle\CoreBundle\DataGrid\Column\ColumnCollection;
 use WellCommerce\Bundle\CoreBundle\DataGrid\Column\ColumnInterface;
+use WellCommerce\Bundle\CoreBundle\DataGrid\Column\Options\Appearance;
+use WellCommerce\Bundle\CoreBundle\DataGrid\Column\Options\Filter;
+use WellCommerce\Bundle\CoreBundle\DataGrid\Column\Options\Sorting;
 use WellCommerce\Bundle\CoreBundle\DataGrid\DataGridInterface;
 
 /**
@@ -28,35 +31,26 @@ class PaymentMethodDataGrid extends AbstractDataGrid implements DataGridInterfac
     /**
      * {@inheritdoc}
      */
-    public function addColumns(ColumnCollection $collection)
+    public function configureColumns(ColumnCollection $collection)
     {
         $collection->add(new Column([
             'id'         => 'id',
-            'source'     => 'payment_method.id',
             'caption'    => $this->trans('payment.id'),
-            'sorting'    => [
-                'default_order' => ColumnInterface::SORT_DIR_DESC,
-            ],
-            'appearance' => [
+            'sorting'    => new Sorting([
+                'default_order' => Sorting::SORT_DIR_DESC,
+            ]),
+            'appearance' => new Appearance([
                 'width'   => 90,
                 'visible' => false,
-            ],
-            'filter'     => [
-                'type' => ColumnInterface::FILTER_BETWEEN,
-            ],
+            ]),
+            'filter'     => new Filter([
+                'type' => Filter::FILTER_BETWEEN,
+            ]),
         ]));
 
         $collection->add(new Column([
-            'id'         => 'name',
-            'source'     => 'payment_method_translation.name',
-            'caption'    => $this->trans('payment.name'),
-            'appearance' => [
-                'width' => 70,
-                'align' => ColumnInterface::ALIGN_LEFT,
-            ],
-            'filter'     => [
-                'type' => ColumnInterface::FILTER_INPUT,
-            ],
+            'id'      => 'name',
+            'caption' => $this->trans('payment.name')
         ]));
     }
 }
