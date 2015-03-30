8b6176c4ef85339f2bf13c3241d2c690f84107cb
Adam Piotrowski <adam@gekosale.com>
2015-03-30 10:28:14 +0200

    Migrations

---
+++ app/sql/migrations/Version20150330005254.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace WellCommerce\Migration;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20150330005254 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE package DROP package_vendor');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE package ADD package_vendor VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci');
+    }
+}
