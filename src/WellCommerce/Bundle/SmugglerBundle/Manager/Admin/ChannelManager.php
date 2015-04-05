fddbe502bde3052dd6b0225ca7aa06453069a033
Adam Piotrowski <adam@wellcommerce.org>
2015-04-05 21:49:50 +0200

    Channel manager

---
+++ src/WellCommerce/Bundle/SmugglerBundle/Manager/Admin/ChannelManager.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\SmugglerBundle\Manager\Admin;
+
+use Packagist\Api\Client;
+use Packagist\Api\Result\Package as RemotePackage;
+use WellCommerce\Bundle\AdminBundle\Manager\AbstractAdminManager;
+use WellCommerce\Bundle\SmugglerBundle\Entity\Package;
+
+/**
+ * Class ChannelManager
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class ChannelManager extends AbstractAdminManager
+{
+
+}
