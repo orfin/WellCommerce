b7bf4e9c7c8190b0c28ffa4f0b27007dcb513dad
Adam Piotrowski <adam@wellcommerce.org>
2015-05-02 22:11:53 +0200

    Changed category provider

---
+++ src/WellCommerce/Bundle/CategoryBundle/Provider/CategoryProvider.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\CategoryBundle\Provider;
+
+use WellCommerce\Bundle\CategoryBundle\Entity\Category;
+use WellCommerce\Bundle\CoreBundle\Provider\AbstractProvider;
+use WellCommerce\Bundle\DataSetBundle\CollectionBuilder\CollectionBuilderFactoryInterface;
+
+/**
+ * Class CategoryProvider
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class CategoryProvider extends AbstractProvider
+{
+}
