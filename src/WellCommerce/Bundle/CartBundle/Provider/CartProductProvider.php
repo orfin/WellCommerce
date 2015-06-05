a4ae8ff93a97a3721c25ab1eb14e6cc0886a500f
Adam Piotrowski <adam@wellcommerce.org>
2015-06-05 03:01:46 +0200

    CartProductProvider

---
+++ src/WellCommerce/Bundle/CartBundle/Provider/CartProductProvider.php
@@ -0,0 +1,95 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\CartBundle\Provider;
+
+use WellCommerce\Bundle\CoreBundle\Provider\AbstractProvider;
+use WellCommerce\Bundle\IntlBundle\Converter\CurrencyConverterInterface;
+
+/**
+ * Class CartProductProvider
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class CartProductProvider extends AbstractProvider implements CartProductProviderInterface
+{
+    /**
+     * @var null|array
+     */
+    protected $dataset = null;
+
+    /**
+     * @var CurrencyConverterInterface
+     */
+    protected $converter;
+
+    /**
+     * @param CurrencyConverterInterface $converter
+     */
+    public function setCurrencyConverter(CurrencyConverterInterface $converter)
+    {
+        $this->converter = $converter;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getProducts()
+    {
+        return $this->getDataSet();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSummary()
+    {
+        $dataset  = $this->getDataSet();
+        $products = $dataset['rows'];
+        $weight   = 0;
+        $price    = 0;
+        $quantity = 0;
+
+        foreach ($products as $product) {
+            $weight += $product['weight'];
+            $price += $this->processPrice($product);
+            $quantity += $product['quantity'];
+        }
+
+        return [
+            'weight'   => $weight,
+            'price'    => $price,
+            'quantity' => $quantity,
+        ];
+    }
+
+    protected function getDataSet()
+    {
+        if (null === $this->dataset) {
+            $this->dataset = $this->getCollectionBuilder()->getDataSet([
+                'order_by' => 'name',
+            ]);
+        }
+
+        return $this->dataset;
+    }
+
+    /**
+     * @param array $product
+     */
+    protected function processPrice(array $product)
+    {
+        $quantityPrice = $product['price'] * $product['quantity'];
+
+        return $this->converter->convert($quantityPrice, $product['currency']);
+    }
+}
