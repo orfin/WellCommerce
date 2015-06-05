b368c8c6c30d3a2605787c37d3f25db3029161cb
Adam Piotrowski <adam@wellcommerce.org>
2015-06-05 11:21:58 +0200

    Translations purger

---
+++ src/WellCommerce/Bundle/IntlBundle/Purger/DictionaryPurger.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\IntlBundle\Purger;
+
+
+use WellCommerce\Bundle\CartBundle\Repository\CartRepositoryInterface;
+use WellCommerce\Bundle\CoreBundle\Helper\Doctrine\DoctrineHelperInterface;
+use WellCommerce\Bundle\CoreBundle\Purger\AbstractPurger;
+use WellCommerce\Bundle\CoreBundle\Purger\PurgerInterface;
+
+class DictionaryPurger extends AbstractPurger implements PurgerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function purge()
+    {
+        return $this->helper->truncateTable('WellCommerce\Bundle\IntlBundle\Entity\Dictionary');
+    }
+}
