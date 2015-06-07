f1d09aa2ba05a2b5bd7d7bfd441856f11513f01c
Adam Piotrowski <adam@wellcommerce.org>
2015-06-08 01:24:01 +0200

    Removed design files

--- web/themes/breadcrumbs.php
+++
@@ -1,6 +0,0 @@
-<ul class="breadcrumbs">
-	<li>Smart TVs</li>
-	<li><a href="category.php">Electronics</a></li>
-	<li class="divider">/</li>
-	<li><a href="category.php">RTV</a></li>
-</ul>
--- web/themes/clientmenubox.php
+++
@@ -1,8 +0,0 @@
-<div class="box">	
-	<div class="list-group cms-categories">
-		<a href="cms.php" class="list-group-item active">Account settings</a>
-		<a href="cms.php" class="list-group-item">Client address</a>
-		<a href="cms.php" class="list-group-item">Orders</a>
-		<a href="cms.php" class="list-group-item">Wish list</a>
-	</div>
-</div>
