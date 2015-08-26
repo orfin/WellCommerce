f11d43371ff556cb2ba2f47941156e76c7f0a26c
Adam Piotrowski <adam@wellcommerce.org>
2015-08-26 20:06:13 +0200

    Unit tests for TaxBundle

---
+++ src/WellCommerce/Bundle/TaxBundle/Tests/Manager/Admin/TaxManagerTest.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\TaxBundle\Tests\Manager\Admin;
+
+use WellCommerce\Bundle\CoreBundle\Test\AbstractTestCase;
+use WellCommerce\Bundle\CoreBundle\Test\Manager\Admin\AbstractAdminManagerTestCase;
+
+/**
+ * Class TaxManagerTest
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class TaxManagerTest extends AbstractAdminManagerTestCase
+{
+    protected function getService()
+    {
+        return $this->container->get('tax.manager.admin');
+    }
+
+    protected function getServiceClassName()
+    {
+        return 'WellCommerce\Bundle\TaxBundle\Manager\Admin\TaxManager';
+    }
+
+    protected function getFormBuilderClassName()
+    {
+        return 'WellCommerce\Bundle\TaxBundle\Form\TaxFormBuilder';
+    }
+
+    protected function getDataGridClassName()
+    {
+        return 'WellCommerce\Bundle\TaxBundle\DataGrid\TaxDataGrid';
+    }
+
+    protected function getRepositoryInterfaceName()
+    {
+        return 'WellCommerce\Bundle\TaxBundle\Repository\TaxRepositoryInterface';
+    }
+}
