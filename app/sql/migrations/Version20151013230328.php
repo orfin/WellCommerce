ae0b281be12492ae8a1b0437c97fa7ebf3c603af
Adrian Potępa <adrian@wellcommerce.org>
2015-10-23 15:38:01 +0200

    Front-end fixes

---
+++ app/sql/migrations/Version20151013230328.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace WellCommerce\Migration;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20151013230328 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE tax CHANGE value value NUMERIC(15, 2) NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE tax CHANGE value value NUMERIC(15, 4) NOT NULL');
+    }
+}
