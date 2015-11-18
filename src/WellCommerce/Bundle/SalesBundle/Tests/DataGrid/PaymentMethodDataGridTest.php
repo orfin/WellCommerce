0f3beedf757a545f902fe00e3301915796a3402e
Adam Piotrowski <adam@wellcommerce.org>
2015-11-18 22:41:52 +0100

    SalesBundle tests

---
+++ src/WellCommerce/Bundle/SalesBundle/Tests/DataGrid/PaymentMethodDataGridTest.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\SalesBundle\Tests\DataGrid;
+
+use WellCommerce\Bundle\CoreBundle\Test\DataGrid\AbstractDataGridTestCase;
+
+/**
+ * Class PaymentMethodDataGridTest
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class PaymentMethodDataGridTest extends AbstractDataGridTestCase
+{
+    protected function getDataGrid()
+    {
+        return $this->container->get('payment_method.datagrid')->getInstance();
+    }
+
+    protected function getColumns()
+    {
+        return [
+            'id',
+            'name',
+            'processor',
+        ];
+    }
+}
