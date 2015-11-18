b3ca352af0652c7a231a5dc0e53305028e35b29a
Adam Piotrowski <adam@wellcommerce.org>
2015-11-18 11:23:34 +0100

    Moved order controllers deeper in folder structure

--- src/WellCommerce/Bundle/SalesBundle/Controller/Front/Order/ConfirmationController.php
+++ src/WellCommerce/Bundle/SalesBundle/Controller/Front/Order/ConfirmationController.php
@@ -10,18 +10,18 @@
  * please view the LICENSE file that was distributed with this source code.
  */
 
-namespace WellCommerce\Bundle\SalesBundle\Controller\Front;
+namespace WellCommerce\Bundle\SalesBundle\Controller\Front\Order;
 
 use WellCommerce\Bundle\CommonBundle\Breadcrumb\BreadcrumbItem;
 use WellCommerce\Bundle\CoreBundle\Controller\Front\AbstractFrontController;
 use WellCommerce\Bundle\CoreBundle\Controller\Front\FrontControllerInterface;
 
 /**
- * Class OrderConfirmationController
+ * Class ConfirmationController
  *
  * @author  Adam Piotrowski <adam@wellcommerce.org>
  */
-class OrderConfirmationController extends AbstractFrontController implements FrontControllerInterface
+class ConfirmationController extends AbstractFrontController implements FrontControllerInterface
 {
     /**
      * @var \WellCommerce\Bundle\SalesBundle\Manager\Front\OrderConfirmationManager
