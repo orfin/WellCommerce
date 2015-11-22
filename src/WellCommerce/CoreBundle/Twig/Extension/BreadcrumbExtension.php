3d5ee01475133c9e1c6f856a7d834c17d66dd4ac
Adam Piotrowski <adam@wellcommerce.org>
2015-11-22 21:55:47 +0100

    Moved breadcrumb extension to core bundle

--- src/WellCommerce/CoreBundle/Twig/Extension/BreadcrumbExtension.php
+++ src/WellCommerce/CoreBundle/Twig/Extension/BreadcrumbExtension.php
@@ -9,7 +9,7 @@
  * For the full copyright and license information,
  * please view the LICENSE file that was distributed with this source code.
  */
-namespace WellCommerce\CommonBundle\Twig\Extension;
+namespace WellCommerce\CoreBundle\Twig\Extension;
 
 use WellCommerce\CoreBundle\Service\Breadcrumb\BreadcrumbItemCollectionInterface;
 
