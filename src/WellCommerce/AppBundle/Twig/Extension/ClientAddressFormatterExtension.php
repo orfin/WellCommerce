9cf382ba6bdb88e7caf998555367925bf37407b5
Adam Piotrowski <adam@wellcommerce.org>
2015-11-23 02:34:30 +0100

    Moved formatter extension deeper in structure

--- src/WellCommerce/AppBundle/Twig/Extension/ClientAddressFormatterExtension.php
+++ src/WellCommerce/AppBundle/Twig/Extension/ClientAddressFormatterExtension.php
@@ -9,7 +9,7 @@
  * For the full copyright and license information,
  * please view the LICENSE file that was distributed with this source code.
  */
-namespace WellCommerce\AppBundle\Twig;
+namespace WellCommerce\AppBundle\Twig\Extension;
 
 use WellCommerce\AppBundle\Entity\ClientBillingAddressInterface;
 use WellCommerce\AppBundle\Entity\ClientContactDetailsInterface;
