567853f25e433652624b215e66e39ac7dced3f24
Adam Piotrowski <adam@wellcommerce.org>
2015-11-24 23:33:10 +0100

    Moved twig extensions

--- src/WellCommerce/CoreBundle/Twig/Extension/ImageExtension.php
+++ src/WellCommerce/CoreBundle/Twig/Extension/ImageExtension.php
@@ -9,7 +9,7 @@
  * For the full copyright and license information,
  * please view the LICENSE file that was distributed with this source code.
  */
-namespace WellCommerce\AppBundle\Twig\Extension;
+namespace WellCommerce\CoreBundle\Twig\Extension;
 
 use WellCommerce\CoreBundle\Helper\Image\ImageHelperInterface;
 
