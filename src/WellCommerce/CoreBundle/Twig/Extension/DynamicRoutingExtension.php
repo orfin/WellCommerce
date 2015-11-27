0d65bdad5ad1e3d510ddea3351e5bba1a80ebd2c
Adam Piotrowski <adam@wellcommerce.org>
2015-11-27 12:12:24 +0100

    Moved extensions to core bundle

--- src/WellCommerce/CoreBundle/Twig/Extension/DynamicRoutingExtension.php
+++ src/WellCommerce/CoreBundle/Twig/Extension/DynamicRoutingExtension.php
@@ -10,7 +10,7 @@
  * please view the LICENSE file that was distributed with this source code.
  */
 
-namespace WellCommerce\AppBundle\Twig\Extension;
+namespace WellCommerce\CoreBundle\Twig\Extension;
 
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use WellCommerce\CoreBundle\Helper\Request\RequestHelperInterface;
