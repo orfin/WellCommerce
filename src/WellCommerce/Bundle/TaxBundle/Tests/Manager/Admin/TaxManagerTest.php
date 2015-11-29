6d73755e4969011c2ad979994a10b9a7c3cede34
Adam Piotrowski <adam@wellcommerce.org>
2015-11-29 21:01:21 +0100

    [STree] Added stree 'taxbundle' in src/WellCommerce/Bundle/TaxBundle

---
+++ src/WellCommerce/Bundle/TaxBundle/Tests/Manager/Admin/TaxManagerTest.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\TaxBundle\Tests\Manager\Admin;
+
+use WellCommerce\Bundle\CoreBundle\Test\Manager\Admin\AbstractAdminManagerTestCase;
+
+/**
+ * Class TaxManagerTest
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class TaxManagerTest extends AbstractAdminManagerTestCase
+{
+    protected function get()
+    {
+        return $this->container->get('tax.manager.admin');
+    }
+
+    protected function getServiceClassName()
+    {
+        return 'WellCommerce\Bundle\AppBundle\Manager\Admin\TaxManager';
+    }
+
+    protected function getFormBuilderClassName()
+    {
+        return 'WellCommerce\Bundle\AppBundle\Form\Admin\TaxFormBuilder';
+    }
+
+    protected function getDataGridClassName()
+    {
+        return 'WellCommerce\Bundle\AppBundle\DataGrid\TaxDataGrid';
+    }
+
+    protected function getRepositoryInterfaceName()
+    {
+        return 'WellCommerce\Bundle\AppBundle\Repository\TaxRepositoryInterface';
+    }
+}
