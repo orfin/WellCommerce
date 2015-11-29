3b9c5173492d71d2c344b396c2d1ac7c55c65b1b
Adam Piotrowski <adam@wellcommerce.org>
2015-11-30 00:54:05 +0100

    Split controllers, commands
    (cherry picked from commit 76834dcd7ab6dd38adaae9a359db85faed31ead4)

---
+++ Factory/CouponFactory.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\AppBundle\Factory;
+
+use WellCommerce\Bundle\CoreBundle\Factory\AbstractFactory;
+use WellCommerce\Bundle\AppBundle\Entity\Coupon;
+
+/**
+ * Class CouponFactory
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class CouponFactory extends AbstractFactory
+{
+    /**
+     * @return \WellCommerce\Bundle\AppBundle\Entity\CouponInterface
+     */
+    public function create()
+    {
+        $coupon = new Coupon();
+
+        return $coupon;
+    }
+}
