716fc0eddfd79130e13aef61e8fea1c649c84e3c
Adam Piotrowski <adam@wellcommerce.org>
2015-11-29 11:01:56 +0100

    [STree] Added stree 'dataset' in src/WellCommerce/Component/DataSet

---
+++ src/WellCommerce/Component/DataSet/Context/DataGridContext.php
@@ -0,0 +1,82 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Component\DataSet\Context;
+
+use Doctrine\ORM\Query;
+use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use WellCommerce\Component\DataSet\Column\ColumnCollection;
+use WellCommerce\Component\DataSet\Paginator\DataSetPaginatorInterface;
+use WellCommerce\Component\DataSet\Request\DataSetRequestInterface;
+
+/**
+ * Class DataGridContext
+ *
+ * @author Adam Piotrowski <adam@wellcommerce.org>
+ */
+class DataGridContext extends AbstractDataSetContext implements DataSetContextInterface
+{
+    /**
+     * @var DataSetPaginatorInterface
+     */
+    protected $paginator;
+
+    /**
+     * Constructor
+     *
+     * @param DataSetPaginatorInterface $paginator
+     */
+    public function __construct(DataSetPaginatorInterface $paginator)
+    {
+        $this->paginator = $paginator;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getResult(QueryBuilder $queryBuilder, DataSetRequestInterface $request, ColumnCollection $columns)
+    {
+        $total    = $this->paginator->getTotalRows($queryBuilder, $columns);
+        $result   = parent::getResult($queryBuilder, $request, $columns);
+        $filtered = ($request->getConditions()->count() !== 0) ? count($result) : $total;
+
+        return [
+            'data_id'       => $this->options['data_id'],
+            'rows_num'      => $total,
+            'starting_from' => $request->getOffset(),
+            'total'         => $total,
+            'filtered'      => $filtered,
+            'rows'          => $result
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+
+        $resolver->setRequired([
+            'data_id',
+        ]);
+
+        $resolver->setDefaults([
+            'data_id' => 0,
+        ]);
+
+        $resolver->setAllowedTypes([
+            'data_id' => ['numeric'],
+        ]);
+    }
+}
