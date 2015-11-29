41de5cacfd07bbc04bfd5dd0e1cefaad7274e574
Adam Piotrowski <adam@wellcommerce.org>
2015-11-30 00:54:05 +0100

    Split controllers, commands
    (cherry picked from commit 76834dcd7ab6dd38adaae9a359db85faed31ead4)

---
+++ Service/Routing/Generator/ProducerRouteGenerator.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\AppBundle\Service\Routing\Generator;
+
+/**
+ * Class ProducerRouteGenerator
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class ProducerRouteGenerator extends AbstractRouteGenerator
+{
+    const GENERATOR_STRATEGY = 'producer';
+
+    public function supports($strategy)
+    {
+        return self::GENERATOR_STRATEGY === $strategy;
+    }
+}
