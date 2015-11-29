f92d757007d80a2977693ccf4153971a9f653fbf
Adam Piotrowski <adam@wellcommerce.org>
2015-11-29 23:40:38 +0100

    Moved media classes
    (cherry picked from commit 521bb3f3d1ce370104bcf2a1674629c7a09b903d)

---
+++ Factory/MediaFactory.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\AppBundle\Factory;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use WellCommerce\Bundle\AppBundle\Entity\Media;
+use WellCommerce\Bundle\CoreBundle\Factory\AbstractFactory;
+
+/**
+ * Class MediaFactory
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class MediaFactory extends AbstractFactory
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function create()
+    {
+        $media = new Media();
+
+        return $media;
+    }
+}
