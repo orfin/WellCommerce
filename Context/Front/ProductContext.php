a935f3d7e5d438d42193f57747d19ec0e658f1ba
Adam Piotrowski <adam@wellcommerce.org>
2015-11-30 00:54:05 +0100

    Split controllers, commands
    (cherry picked from commit 76834dcd7ab6dd38adaae9a359db85faed31ead4)

---
+++ Context/Front/ProductContext.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\AppBundle\Context\Front;
+
+use WellCommerce\Bundle\AppBundle\Entity\ProductInterface;
+
+/**
+ * Class ProductContext
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class ProductContext implements ProductContextInterface
+{
+    /**
+     * @var ProductInterface
+     */
+    protected $currentProduct;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setCurrentProduct(ProductInterface $product)
+    {
+        $this->currentProduct = $product;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCurrentProduct()
+    {
+        return $this->currentProduct;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function hasCurrentProduct()
+    {
+        return $this->currentProduct instanceof ProductInterface;
+    }
+
+}
