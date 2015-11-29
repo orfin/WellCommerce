5cfbed081ad434367f322145ef20b6e5348cdfe4
Adam Piotrowski <adam@wellcommerce.org>
2015-11-30 00:38:40 +0100

    Splitting bundles
    (cherry picked from commit d730e4a7379c98cf04ac63a906aaa40281473093)

---
+++ Repository/PaymentMethodRepositoryInterface.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\AppBundle\Repository;
+
+use WellCommerce\Bundle\CoreBundle\Repository\RepositoryInterface;
+
+/**
+ * Interface PaymentMethodRepositoryInterface
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+interface PaymentMethodRepositoryInterface extends RepositoryInterface
+{
+    /**
+     * Returns default shipping method
+     *
+     * @return null|\WellCommerce\Bundle\AppBundle\Entity\PaymentMethodInterface
+     */
+    public function getDefaultPaymentMethod();
+}
