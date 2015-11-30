cf25fddcf3701cdb9e009b204315e4c10c57b5ea
Adam Piotrowski <adam@wellcommerce.org>
2015-11-30 02:10:03 +0100

    Split twig extensions
    (cherry picked from commit 07ab23470735dcad904ea2ccc5f4c15446d4a65c)

---
+++ Twig/Extension/ProductReviewExtension.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+namespace WellCommerce\Bundle\AppBundle\Twig\Extension;
+
+use Doctrine\Common\Collections\Collection;
+use WellCommerce\Bundle\AppBundle\Entity\ProductReviewInterface;
+
+/**
+ * Class ProductReviewExtension
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class ProductReviewExtension extends \Twig_Extension
+{
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('productReviewAverage', [$this, 'getProductReviewAverage'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'product_review';
+    }
+
+    /**
+     * Returns product statuses
+     *
+     * @param int    $limit
+     * @param string $orderBy
+     * @param string $orderDir
+     *
+     * @return array
+     */
+    public function getProductReviewAverage(Collection $collection)
+    {
+        $totalRating  = 0;
+        $reviewsTotal = $collection->count();
+
+        $collection->map(function (ProductReviewInterface $review) use (&$totalRating) {
+            $totalRating += $review->getRating();
+        });
+
+        return ($reviewsTotal > 0) ? round($totalRating / $reviewsTotal, 2) : 0;
+    }
+}
