03d94b92454a278cc7d0d9032ce5e9a7c59f7bd3
Adam Piotrowski <adam@wellcommerce.org>
2015-11-30 15:20:49 +0100

    Moved factories
    (cherry picked from commit 802c4ff68703094f20b03007bba138f467742ae2)

---
+++ Factory/DictionaryFactory.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\AppBundle\Factory;
+
+use WellCommerce\Bundle\AppBundle\Entity\Dictionary;
+use WellCommerce\Bundle\CoreBundle\Factory\AbstractFactory;
+
+/**
+ * Class DictionaryFactory
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class DictionaryFactory extends AbstractFactory
+{
+    /**
+     * @return \WellCommerce\Bundle\AppBundle\Entity\DictionaryInterface
+     */
+    public function create()
+    {
+        $dictionary = new Dictionary();
+
+        return $dictionary;
+    }
+}
