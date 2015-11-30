d3bba5d9e54d6f92f1b77c66a448ea03c04124cb
Adam Piotrowski <adam@wellcommerce.org>
2015-11-30 13:43:32 +0100

    Moved entities
    (cherry picked from commit 40d04d95ba3d38d0f6a6a474fefb03de93d18857)

---
+++ Repository/DictionaryRepository.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+namespace WellCommerce\Bundle\AppBundle\Repository;
+
+use WellCommerce\Bundle\CoreBundle\Repository\AbstractEntityRepository;
+
+/**
+ * Class DictionaryRepository
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class DictionaryRepository extends AbstractEntityRepository implements DictionaryRepositoryInterface
+{
+    /**
+     * @var array
+     */
+    private $domains = ['admin', 'front', 'flashes', 'validators'];
+
+    /**
+     * Returns possible translation domains
+     *
+     * @return array
+     */
+    public function getTranslationDomains()
+    {
+        $domains = [];
+        foreach ($this->domains as $domain) {
+            $domains[$domain] = $domain;
+        }
+
+        return $domains;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDictionaryTranslations()
+    {
+        $qb = parent::getQueryBuilder()
+            ->leftJoin('WellCommerce\Bundle\AppBundle\Entity\DictionaryTranslation',
+                'dictionary_translation',
+                'WITH',
+                'dictionary.id = dictionary_translation.translatable')
+            ->groupBy('dictionary.identifier, dictionary_translation.locale')
+            ->select('dictionary.identifier,dictionary_translation.locale,dictionary_translation.translation');
+
+        $query = $qb->getQuery();
+
+        return $query->getArrayResult();
+    }
+}
