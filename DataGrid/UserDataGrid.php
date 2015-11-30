38307f30f35ee72fbefc6674386b430d032101e0
Adam Piotrowski <adam@wellcommerce.org>
2015-11-30 02:05:42 +0100

    Split datagrids
    (cherry picked from commit 706cef5e4b89cd725f71aa3260653c35df9f225b)

---
+++ DataGrid/UserDataGrid.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+namespace WellCommerce\Bundle\AppBundle\DataGrid;
+
+use WellCommerce\Bundle\CoreBundle\DataGrid\AbstractDataGrid;
+use WellCommerce\Component\DataGrid\Column\Column;
+use WellCommerce\Component\DataGrid\Column\ColumnCollection;
+use WellCommerce\Component\DataGrid\Column\ColumnInterface;
+use WellCommerce\Component\DataGrid\Column\Options\Appearance;
+use WellCommerce\Component\DataGrid\Column\Options\Filter;
+use WellCommerce\Component\DataGrid\Column\Options\Sorting;
+
+/**
+ * Class UserDataGrid
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class UserDataGrid extends AbstractDataGrid
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function configureColumns(ColumnCollection $collection)
+    {
+        $collection->add(new Column([
+            'id'         => 'id',
+            'caption'    => $this->trans('user.label.id'),
+            'sorting'    => new Sorting([
+                'default_order' => ColumnInterface::SORT_DIR_DESC,
+            ]),
+            'appearance' => new Appearance([
+                'width'   => 90,
+                'visible' => false,
+            ]),
+            'filter'     => new Filter([
+                'type' => Filter::FILTER_BETWEEN,
+            ]),
+        ]));
+
+        $collection->add(new Column([
+            'id'      => 'username',
+            'caption' => $this->trans('user.label.username'),
+        ]));
+
+        $collection->add(new Column([
+            'id'      => 'email',
+            'caption' => $this->trans('user.label.email'),
+        ]));
+
+        $collection->add(new Column([
+            'id'      => 'first_name',
+            'caption' => $this->trans('user.label.first_name'),
+        ]));
+
+        $collection->add(new Column([
+            'id'      => 'last_name',
+            'caption' => $this->trans('user.label.last_name'),
+        ]));
+
+        $collection->add(new Column([
+            'id'      => 'enabled',
+            'caption' => $this->trans('user.label.enabled'),
+        ]));
+    }
+}
