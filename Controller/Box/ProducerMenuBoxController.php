97910becc1adc9a5976084c456bfeed924b04398
Adam Piotrowski <adam@wellcommerce.org>
2015-11-30 13:47:32 +0100

    Moved box controllers
    (cherry picked from commit d4b2e9bc9327672c2786e9bb7f22e433a65040fb)

---
+++ Controller/Box/ProducerMenuBoxController.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\AppBundle\Controller\Box;
+
+use WellCommerce\Bundle\CoreBundle\Controller\Box\AbstractBoxController;
+
+/**
+ * Class ProducerMenuBoxController
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class ProducerMenuBoxController extends AbstractBoxController
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function indexAction()
+    {
+        $producers = $this->get('producer.dataset.front')->getResult('array');
+
+        return $this->displayTemplate('index', [
+            'producers'      => $producers,
+            'activeProducer' => $this->manager->getProducerContext()->getCurrentProducerIdentifier()
+        ]);
+    }
+}
