b324be2bb48d335eaa5e72c59bf3226921963348
Adam Piotrowski <adam@wellcommerce.org>
2015-11-30 13:54:02 +0100

    Moved box & front controllers
    (cherry picked from commit 8dd86d871921217331c3931bab03a537629ad3fe)

---
+++ DataSet/Admin/ProductReviewDataSet.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\AppBundle\DataSet\Admin;
+
+use WellCommerce\Component\DataSet\Configurator\DataSetConfiguratorInterface;
+use WellCommerce\Bundle\CoreBundle\DataSet\AbstractDataSet;
+
+/**
+ * Class ProductReviewDataSet
+ *
+ * @author Adam Piotrowski <adam@wellcommerce.org>
+ */
+class ProductReviewDataSet extends AbstractDataSet
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(DataSetConfiguratorInterface $configurator)
+    {
+        $configurator->setColumns([
+            'id'        => 'product_review.id',
+            'nick'      => 'product_review.nick',
+            'rating'    => 'product_review.rating',
+            'createdAt' => 'product_review.createdAt',
+            'product'   => 'product_translation.name',
+        ]);
+
+        $configurator->setColumnTransformers([
+            'createdAt' => $this->getDataSetTransformer('date', ['format' => 'Y-m-d H:i:s']),
+        ]);
+    }
+}
