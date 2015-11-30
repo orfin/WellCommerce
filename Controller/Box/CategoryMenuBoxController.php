96ce955f06a353747279b9b60f54b9ff73668854
Adam Piotrowski <adam@wellcommerce.org>
2015-11-30 02:05:42 +0100

    Split datagrids
    (cherry picked from commit 706cef5e4b89cd725f71aa3260653c35df9f225b)

---
+++ Controller/Box/CategoryMenuBoxController.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\AppBundle\Controller\Box;
+
+use WellCommerce\Bundle\CoreBundle\Controller\Box\AbstractBoxController;
+
+/**
+ * Class CategoryMenuBoxController
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class CategoryMenuBoxController extends AbstractBoxController
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function indexAction()
+    {
+        return $this->displayTemplate('index', [
+            'active' => $this->manager->getCategoryContext()->getCurrentCategoryIdentifier()
+        ]);
+    }
+}
