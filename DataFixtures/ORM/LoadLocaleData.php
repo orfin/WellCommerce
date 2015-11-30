494a3509168d39934a29b9ab045c6e3345d9a4ba
Adam Piotrowski <adam@wellcommerce.org>
2015-11-30 02:05:42 +0100

    Split datagrids
    (cherry picked from commit 706cef5e4b89cd725f71aa3260653c35df9f225b)

---
+++ DataFixtures/ORM/LoadLocaleData.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use WellCommerce\Bundle\AppBundle\Entity\Locale;
+use WellCommerce\Bundle\CoreBundle\DataFixtures\AbstractDataFixture;
+
+/**
+ * Class LoadLocaleData
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class LoadLocaleData extends AbstractDataFixture
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $currency = $this->getReference('currency_USD');
+
+        $en = new Locale();
+        $en->setCode('en');
+        $en->setCurrency($currency);
+        $manager->persist($en);
+        $manager->flush();
+
+        $this->setReference('locale_en', $en);
+    }
+}
