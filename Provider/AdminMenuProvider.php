34c0536197fecde9c832e5c7125e306561b31bed
Adam Piotrowski <adam@wellcommerce.org>
2015-11-30 01:22:16 +0100

    Splitted repositories
    (cherry picked from commit 8e2049361ade4debaa49035a1986ed66141ae9b1)

---
+++ Provider/AdminMenuProvider.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\AppBundle\Provider;
+
+use Doctrine\Common\Collections\Criteria;
+use WellCommerce\Bundle\AppBundle\Repository\AdminMenuRepositoryInterface;
+
+/**
+ * Class AdminMenuProvider
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class AdminMenuProvider
+{
+    /**
+     * @var AdminMenuRepositoryInterface
+     */
+    protected $adminMenuRepository;
+
+    /**
+     * Constructor
+     *
+     * @param AdminMenuRepositoryInterface $adminMenuRepository
+     */
+    public function __construct(AdminMenuRepositoryInterface $adminMenuRepository)
+    {
+        $this->adminMenuRepository = $adminMenuRepository;
+    }
+
+    public function getMenu()
+    {
+        $criteria = new Criteria();
+        $criteria->orderBy(['hierarchy' => 'asc']);
+        $criteria->andWhere($criteria->expr()->eq('parent', null));
+
+        return $this->adminMenuRepository->matching($criteria);
+    }
+}
