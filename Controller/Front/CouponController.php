a0a6e6dab2410ce483f40241f3f84b19d70daa3c
Adam Piotrowski <adam@wellcommerce.org>
2015-11-30 13:54:02 +0100

    Moved box & front controllers
    (cherry picked from commit 8dd86d871921217331c3931bab03a537629ad3fe)

---
+++ Controller/Front/CouponController.php
@@ -0,0 +1,84 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\AppBundle\Controller\Front;
+
+use Symfony\Component\HttpFoundation\Request;
+use WellCommerce\Bundle\CoreBundle\Controller\Front\AbstractFrontController;
+use WellCommerce\Bundle\AppBundle\Exception\CouponException;
+
+/**
+ * Class CouponController
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class CouponController extends AbstractFrontController
+{
+    /**
+     * @var \WellCommerce\Bundle\AppBundle\Manager\Front\CouponManager
+     */
+    protected $manager;
+
+    /**
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse|\Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function addAction(Request $request)
+    {
+        if (!$request->isXmlHttpRequest()) {
+            return $this->redirectToRoute('cart.front.index');
+        }
+
+        $code = $this->getRequestHelper()->getRequestBagParam('code');
+
+        try {
+            $this->manager->useCoupon($code);
+
+            $result = [
+                'success' => true
+            ];
+        } catch (CouponException $e) {
+            $result = [
+                'error'   => $this->trans('coupon.error'),
+                'message' => $this->trans($e->getMessage()),
+            ];
+        }
+
+        return $this->jsonResponse($result);
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function deleteAction(Request $request)
+    {
+        if (!$request->isXmlHttpRequest()) {
+            return $this->redirectToRoute('cart.front.index');
+        }
+
+        try {
+            $this->manager->removeCartCoupon();
+
+            $result = [
+                'success' => true
+            ];
+        } catch (CouponException $e) {
+            $result = [
+                'error'   => $this->trans('coupon.error'),
+                'message' => $this->trans($e->getMessage()),
+            ];
+        }
+
+        return $this->jsonResponse($result);
+    }
+}
