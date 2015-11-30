caeb317ce5e13ed2466c7b42e8e4312233b05e3c
Adam Piotrowski <adam@wellcommerce.org>
2015-11-30 01:01:53 +0100

    Split controllers
    (cherry picked from commit a20b55de5ce1881eee19b3de347ea861826a62e1)

---
+++ Controller/Admin/SalesReportController.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\AppBundle\Controller\Admin;
+
+use Carbon\Carbon;
+use DateInterval;
+use WellCommerce\Bundle\CoreBundle\Controller\Admin\AbstractAdminController;
+use WellCommerce\Bundle\AppBundle\Calculator\SalesSummaryCalculator;
+use WellCommerce\Bundle\AppBundle\Configuration\ReportConfiguration;
+use WellCommerce\Bundle\AppBundle\Context\LineChartContext;
+
+/**
+ * Class SalesReportController
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class SalesReportController extends AbstractAdminController
+{
+    public function indexAction()
+    {
+        $salesSummary = $this->getSalesSummary();
+
+        return $this->displayTemplate('index', [
+                'salesChart'   => $this->getSalesChart(),
+                'salesSummary' => $salesSummary->getSummary(),
+                'currency'     => $this->getRequestHelper()->getCurrentCurrency()
+            ]
+        );
+    }
+
+    /**
+     * @return LineChartContext
+     */
+    protected function getSalesChart()
+    {
+        $startDate     = Carbon::now()->startOfMonth();
+        $endDate       = Carbon::now()->endOfMonth();
+        $interval      = new DateInterval('P1D');
+        $configuration = new ReportConfiguration($startDate, $endDate, $interval, 'd', 'Y-m-d');
+        $report        = $this->get('sales_report.provider')->getReport($configuration);
+
+        return new LineChartContext($report, $configuration);
+    }
+
+    protected function getSalesSummary()
+    {
+        $startDate     = Carbon::now()->startOfMonth();
+        $endDate       = Carbon::now()->endOfMonth();
+        $interval      = new DateInterval('P1D');
+        $configuration = new ReportConfiguration($startDate, $endDate, $interval, 'Y-m-d', 'Y-m-d');
+        $report        = $this->get('sales_report.provider')->getReport($configuration);
+
+        return new SalesSummaryCalculator($report, $configuration);
+    }
+}
