24e23c331572ce0fa969ff34f7bb6df5a3645da8
Adam Piotrowski <adam@wellcommerce.org>
2015-11-30 13:43:32 +0100

    Moved entities
    (cherry picked from commit 40d04d95ba3d38d0f6a6a474fefb03de93d18857)

---
+++ Entity/PaymentMethodAwareTrait.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\AppBundle\Entity;
+
+/**
+ * Class PaymentMethodAwareTrait
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+trait PaymentMethodAwareTrait
+{
+    /**
+     * @var PaymentMethodInterface
+     */
+    protected $paymentMethod;
+
+    /**
+     * @return null|PaymentMethodInterface
+     */
+    public function getPaymentMethod()
+    {
+        return $this->paymentMethod;
+    }
+
+    /**
+     * @param null|PaymentMethodInterface $paymentMethod
+     */
+    public function setPaymentMethod(PaymentMethodInterface $paymentMethod = null)
+    {
+        $this->paymentMethod = $paymentMethod;
+    }
+}
---
+++ Entity/PaymentMethodInterface.php
@@ -0,0 +1,76 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\AppBundle\Entity;
+
+use Doctrine\Common\Collections\Collection;
+use WellCommerce\Bundle\AppBundle\Entity\BlameableInterface;
+use WellCommerce\Bundle\AppBundle\Entity\HierarchyAwareInterface;
+use WellCommerce\Bundle\AppBundle\Entity\TimestampableInterface;
+use WellCommerce\Bundle\AppBundle\Entity\TranslatableInterface;
+
+/**
+ * Interface PaymentMethodInterface
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+interface PaymentMethodInterface extends TimestampableInterface, TranslatableInterface, BlameableInterface, HierarchyAwareInterface
+{
+    /**
+     * @return integer
+     */
+    public function getId();
+
+    /**
+     * Returns payment method processor
+     *
+     * @return string
+     */
+    public function getProcessor();
+
+    /**
+     * Sets payment method processor
+     *
+     * @param string $processor
+     */
+    public function setProcessor($processor);
+
+    /**
+     * @return Collection
+     */
+    public function getShippingMethods();
+
+    /**
+     * @param Collection $shippingMethods
+     */
+    public function setShippingMethods(Collection $shippingMethods);
+
+    /**
+     * @return OrderStatusInterface
+     */
+    public function getDefaultOrderStatus();
+
+    /**
+     * @param OrderStatusInterface $defaultOrderStatus
+     */
+    public function setDefaultOrderStatus(OrderStatusInterface $defaultOrderStatus);
+
+    /**
+     * @return Collection
+     */
+    public function getConfiguration();
+
+    /**
+     * @param Collection $configuration
+     */
+    public function setConfiguration(Collection $configuration);
+}
