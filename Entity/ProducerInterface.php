655b459fec6d6cd93f5be439b4674e9188c4faf1
Adam Piotrowski <adam@wellcommerce.org>
2015-11-30 13:43:32 +0100

    Moved entities
    (cherry picked from commit 40d04d95ba3d38d0f6a6a474fefb03de93d18857)

---
+++ Entity/ProducerInterface.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\AppBundle\Entity;
+
+use Doctrine\Common\Collections\Collection;
+use WellCommerce\Bundle\AppBundle\Entity\ShopCollectionAwareInterface;
+use WellCommerce\Bundle\AppBundle\Entity\BlameableInterface;
+use WellCommerce\Bundle\AppBundle\Entity\TimestampableInterface;
+use WellCommerce\Bundle\AppBundle\Entity\TranslatableInterface;
+
+/**
+ * Interface ProducerInterface
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+interface ProducerInterface extends TranslatableInterface, TimestampableInterface, BlameableInterface, ShopCollectionAwareInterface
+{
+    /**
+     * @return integer
+     */
+    public function getId();
+
+    /**
+     * @return Collection|\WellCommerce\Bundle\AppBundle\Entity\ProductInterface[]
+     */
+    public function getProducts();
+
+    /**
+     * @return Collection|\WellCommerce\Bundle\AppBundle\Entity\DelivererInterface[]
+     */
+    public function getDeliverers();
+
+    /**
+     * @param Collection $collection
+     */
+    public function setDeliverers(Collection $collection);
+
+    /**
+     * @param DelivererInterface $deliverer
+     */
+    public function addDeliverer(DelivererInterface $deliverer);
+}
