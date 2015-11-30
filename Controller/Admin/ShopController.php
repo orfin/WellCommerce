036ff990e02435e7a57c3200a64043971ae98a04
Adam Piotrowski <adam@wellcommerce.org>
2015-11-30 01:22:16 +0100

    Splitted repositories
    (cherry picked from commit 8e2049361ade4debaa49035a1986ed66141ae9b1)

---
+++ Controller/Admin/ShopController.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\AppBundle\Controller\Admin;
+
+use WellCommerce\Bundle\CoreBundle\Controller\Admin\AbstractAdminController;
+
+/**
+ * Class ShopController
+ *
+ * @author Adam Piotrowski <adam@wellcommerce.org>
+ */
+class ShopController extends AbstractAdminController
+{
+    /**
+     * Action used to switch admin shop context
+     *
+     * @param int $id
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function changeContextAction($id)
+    {
+        $shop = $this->manager->getRepository()->find($id);
+        $this->get('shop.context.admin')->setCurrentScope($shop);
+
+        return $this->jsonResponse([
+            'success' => true
+        ]);
+    }
+}
