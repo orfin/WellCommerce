b769e8af31f0ce3b37f49e10a8245cbff647cc15
Adam Piotrowski <adam@wellcommerce.org>
2015-11-30 15:20:49 +0100

    Moved factories
    (cherry picked from commit 802c4ff68703094f20b03007bba138f467742ae2)

---
+++ Factory/AvailabilityFactory.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\AppBundle\Factory;
+
+use WellCommerce\Bundle\AppBundle\Entity\Availability;
+use WellCommerce\Bundle\CoreBundle\Factory\AbstractFactory;
+
+/**
+ * Class AvailabilityFactory
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class AvailabilityFactory extends AbstractFactory
+{
+    /**
+     * @return \WellCommerce\Bundle\AppBundle\Entity\AvailabilityInterface
+     */
+    public function create()
+    {
+        $availability = new Availability();
+
+        return $availability;
+    }
+}
