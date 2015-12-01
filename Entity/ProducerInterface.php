b7d35d4fa0c36687528e9a7ba14630892a08720c
Adam Piotrowski <adam@wellcommerce.org>
2015-12-01 13:45:11 +0100

    Base bundles separation
    (cherry picked from commit c16cb0620c590f2e0b6d7a2400a502fb6bde25e2)

--- Entity/ProducerInterface.php
+++ Entity/ProducerInterface.php
@@ -13,10 +13,10 @@
 namespace WellCommerce\Bundle\AppBundle\Entity;
 
 use Doctrine\Common\Collections\Collection;
-use WellCommerce\Bundle\AppBundle\Entity\ShopCollectionAwareInterface;
-use WellCommerce\Bundle\UserBundle\Entity\BlameableInterface;
+use WellCommerce\Bundle\ShopBundle\Entity\ShopCollectionAwareInterface;
+use WellCommerce\Bundle\CoreBundle\Entity\BlameableInterface;
 use WellCommerce\Bundle\CoreBundle\Entity\TimestampableInterface;
-use WellCommerce\Bundle\AppBundle\Entity\TranslatableInterface;
+use WellCommerce\Bundle\CoreBundle\Entity\TranslatableInterface;
 
 /**
  * Interface ProducerInterface
