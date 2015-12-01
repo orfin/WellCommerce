728a2cd05d950b7cb939f17380a7991726a06c44
Adam Piotrowski <adam@wellcommerce.org>
2015-12-01 13:45:11 +0100

    Base bundles separation
    (cherry picked from commit c16cb0620c590f2e0b6d7a2400a502fb6bde25e2)

--- Entity/Product.php
+++ Entity/Product.php
@@ -17,7 +17,7 @@ use Knp\DoctrineBehaviors\Model\Blameable\Blameable;
 use Knp\DoctrineBehaviors\Model\Timestampable\Timestampable;
 use Knp\DoctrineBehaviors\Model\Translatable\Translatable;
 use WellCommerce\Bundle\AppBundle\Entity\Attribute\GroupInterface;
-use WellCommerce\Bundle\AppBundle\Entity\ShopCollectionAwareTrait;
+use WellCommerce\Bundle\ShopBundle\Entity\ShopCollectionAwareTrait;
 use WellCommerce\Bundle\TaxBundle\Entity\TaxInterface;
 use WellCommerce\Bundle\AppBundle\Doctrine\ORM\Behaviours\EnableableTrait;
 use WellCommerce\Bundle\AppBundle\Doctrine\ORM\Behaviours\PhotoTrait;
