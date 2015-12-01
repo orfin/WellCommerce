2de17f22d18205bf4c79a89543a019dcc0dfbc4c
Adam Piotrowski <adam@wellcommerce.org>
2015-12-01 13:45:11 +0100

    Base bundles separation
    (cherry picked from commit c16cb0620c590f2e0b6d7a2400a502fb6bde25e2)

--- Entity/OrderInterface.php
+++ Entity/OrderInterface.php
@@ -17,7 +17,7 @@ use WellCommerce\Bundle\AppBundle\Entity\ClientAwareInterface;
 use WellCommerce\Bundle\AppBundle\Entity\ClientBillingAddressInterface;
 use WellCommerce\Bundle\AppBundle\Entity\ClientContactDetailsInterface;
 use WellCommerce\Bundle\AppBundle\Entity\ClientShippingAddressInterface;
-use WellCommerce\Bundle\AppBundle\Entity\ShopAwareInterface;
+use WellCommerce\Bundle\ShopBundle\Entity\ShopAwareInterface;
 use WellCommerce\Bundle\CoreBundle\Entity\TimestampableInterface;
 use WellCommerce\Bundle\AppBundle\Entity\CouponAwareInterface;
 use WellCommerce\Bundle\AppBundle\Calculator\ShippingCalculatorSubjectInterface;
