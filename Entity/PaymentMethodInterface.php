d8c99f64946890f23ae4418c0cc40eec4f0535d9
Adam Piotrowski <adam@wellcommerce.org>
2015-12-01 13:45:11 +0100

    Base bundles separation
    (cherry picked from commit c16cb0620c590f2e0b6d7a2400a502fb6bde25e2)

--- Entity/PaymentMethodInterface.php
+++ Entity/PaymentMethodInterface.php
@@ -13,10 +13,10 @@
 namespace WellCommerce\Bundle\AppBundle\Entity;
 
 use Doctrine\Common\Collections\Collection;
-use WellCommerce\Bundle\UserBundle\Entity\BlameableInterface;
+use WellCommerce\Bundle\CoreBundle\Entity\BlameableInterface;
 use WellCommerce\Bundle\AppBundle\Entity\HierarchyAwareInterface;
 use WellCommerce\Bundle\CoreBundle\Entity\TimestampableInterface;
-use WellCommerce\Bundle\AppBundle\Entity\TranslatableInterface;
+use WellCommerce\Bundle\CoreBundle\Entity\TranslatableInterface;
 
 /**
  * Interface PaymentMethodInterface
