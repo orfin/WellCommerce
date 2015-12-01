d0092b6fb0f9b989be4c2393a5a54a64ca7459d1
Adam Piotrowski <adam@wellcommerce.org>
2015-12-01 13:45:11 +0100

    Base bundles separation
    (cherry picked from commit c16cb0620c590f2e0b6d7a2400a502fb6bde25e2)

--- Entity/ContactTranslation.php
+++ Entity/ContactTranslation.php
@@ -13,7 +13,7 @@
 namespace WellCommerce\Bundle\AppBundle\Entity;
 
 use Knp\DoctrineBehaviors\Model\Translatable\Translation;
-use WellCommerce\Bundle\AppBundle\Entity\LocaleAwareInterface;
+use WellCommerce\Bundle\LocaleBundle\Entity\LocaleAwareInterface;
 use WellCommerce\Bundle\AppBundle\Doctrine\ORM\Behaviours\AddressTrait;
 
 /**
