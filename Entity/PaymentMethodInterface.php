b080e12c54e86ed73bc4f2adae6a349df6ad977f
Adam Piotrowski <adam@wellcommerce.org>
2015-12-01 02:18:53 +0100

    AppBundle fixes
    (cherry picked from commit 2cd8a65e4ffa8292ba574c6a8d76d10cb1a56df7)

--- Entity/PaymentMethodInterface.php
+++ Entity/PaymentMethodInterface.php
@@ -13,9 +13,9 @@
 namespace WellCommerce\Bundle\AppBundle\Entity;
 
 use Doctrine\Common\Collections\Collection;
-use WellCommerce\Bundle\AppBundle\Entity\BlameableInterface;
+use WellCommerce\Bundle\UserBundle\Entity\BlameableInterface;
 use WellCommerce\Bundle\AppBundle\Entity\HierarchyAwareInterface;
-use WellCommerce\Bundle\AppBundle\Entity\TimestampableInterface;
+use WellCommerce\Bundle\CoreBundle\Entity\TimestampableInterface;
 use WellCommerce\Bundle\AppBundle\Entity\TranslatableInterface;
 
 /**
