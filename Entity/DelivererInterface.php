185b5a8894776d7b3a87d8eb9f0620d33e7ea3fb
Adam Piotrowski <adam@wellcommerce.org>
2015-12-01 02:18:53 +0100

    AppBundle fixes
    (cherry picked from commit 2cd8a65e4ffa8292ba574c6a8d76d10cb1a56df7)

--- Entity/DelivererInterface.php
+++ Entity/DelivererInterface.php
@@ -13,8 +13,8 @@
 namespace WellCommerce\Bundle\AppBundle\Entity;
 
 use Doctrine\Common\Collections\Collection;
-use WellCommerce\Bundle\AppBundle\Entity\BlameableInterface;
-use WellCommerce\Bundle\AppBundle\Entity\TimestampableInterface;
+use WellCommerce\Bundle\UserBundle\Entity\BlameableInterface;
+use WellCommerce\Bundle\CoreBundle\Entity\TimestampableInterface;
 use WellCommerce\Bundle\AppBundle\Entity\TranslatableInterface;
 
 /**
