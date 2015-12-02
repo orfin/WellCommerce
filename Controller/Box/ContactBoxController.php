6067cbadc62a7a97ddbe100c0c50707b4164f2b9
Adam Piotrowski <adam@wellcommerce.org>
2015-12-02 22:14:42 +0100

    Core bundle fixes
    (cherry picked from commit 8940d721df6eb57aabbb4c2e9305d6b817bab5d1)

--- Controller/Box/ContactBoxController.php
+++ Controller/Box/ContactBoxController.php
@@ -10,7 +10,7 @@
  * please view the LICENSE file that was distributed with this source code.
  */
 
-namespace WellCommerce\Bundle\AppBundle\Controller\Box;
+namespace WellCommerce\Bundle\ContactBundle\Controller\Box;
 
 use WellCommerce\Bundle\CoreBundle\Controller\Box\AbstractBoxController;
 
