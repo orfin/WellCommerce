fcf4b617e801b972b28af6ce235e10884a7884a2
Adam Piotrowski <adam@wellcommerce.org>
2015-12-05 00:01:36 +0100

    Optimized imports
    (cherry picked from commit e4c71de80a7a95a3b8af880e96fea9d00f3f7c1b)

--- Factory/DictionaryFactory.php
+++ Factory/DictionaryFactory.php
@@ -12,8 +12,8 @@
 
 namespace WellCommerce\Bundle\DictionaryBundle\Factory;
 
-use WellCommerce\Bundle\DictionaryBundle\Entity\Dictionary;
 use WellCommerce\Bundle\CoreBundle\Factory\AbstractFactory;
+use WellCommerce\Bundle\DictionaryBundle\Entity\Dictionary;
 
 /**
  * Class DictionaryFactory
