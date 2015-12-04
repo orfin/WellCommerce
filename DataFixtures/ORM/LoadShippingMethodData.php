0c9c5cddc0f81bc2669fcc019c81f4d2d27c76fa
Adam Piotrowski <adam@wellcommerce.org>
2015-12-04 13:57:07 +0100

    Finished restructurisation of bundles
    (cherry picked from commit 7fa3658009b1af428e1d2bcf9ad5b4a493bfe79c)

--- DataFixtures/ORM/LoadShippingMethodData.php
+++ DataFixtures/ORM/LoadShippingMethodData.php
@@ -14,12 +14,12 @@ namespace WellCommerce\Bundle\ShippingBundle\DataFixtures\ORM;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Persistence\ObjectManager;
-use WellCommerce\Bundle\AppBundle\DataFixtures\ORM\LoadCurrencyData;
-use WellCommerce\Bundle\AppBundle\DataFixtures\ORM\LoadTaxData;
 use WellCommerce\Bundle\CoreBundle\DataFixtures\AbstractDataFixture;
 use WellCommerce\Bundle\AppBundle\Entity\Price;
+use WellCommerce\Bundle\CurrencyBundle\DataFixtures\ORM\LoadCurrencyData;
 use WellCommerce\Bundle\ShippingBundle\Entity\ShippingMethodCost;
 use WellCommerce\Bundle\ShippingBundle\Entity\ShippingMethodInterface;
+use WellCommerce\Bundle\TaxBundle\DataFixtures\ORM\LoadTaxData;
 
 /**
  * Class LoadShippingData
