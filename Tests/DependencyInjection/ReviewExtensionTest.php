0abaa06b08948e08c8a9654d72b74b84b4b7c7c7
Adam Piotrowski <adam@wellcommerce.org>
2015-12-05 23:57:36 +0100

    Review tests
    (cherry picked from commit d7b2d5c1b8bd9629993c5177df64a7d64fd99263)

---
+++ Tests/DependencyInjection/ReviewExtensionTest.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\ReviewBundle\Tests\DependencyInjection;
+
+use WellCommerce\Bundle\CoreBundle\Test\DependencyInjection\AbstractExtensionTestCase;
+
+/**
+ * Class ReviewExtensionTest
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class ReviewExtensionTest extends AbstractExtensionTestCase
+{
+    /**
+     * @return array
+     */
+    public function getRequiredServices()
+    {
+        return [
+            'services' => [
+                [
+                    'review.repository',
+                    'review.factory',
+                    'review.event_dispatcher',
+                    'review.form_builder.admin',
+                    'review.dataset.admin',
+                    'review.datagrid',
+                    'review.controller.admin',
+                ]
+            ],
+        ];
+    }
+}
