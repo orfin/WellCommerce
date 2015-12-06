349253de296ebf9bbbffd913db1e6d681cc179e1
Adam Piotrowski <adam@wellcommerce.org>
2015-12-06 19:20:44 +0100

    Attribute fixes
    (cherry picked from commit 8f0d76f6041aa620d3cb021a2b33f91f4068a39e)

--- DependencyInjection/WellCommerceAppExtension.php
+++ DependencyInjection/WellCommerceAppExtension.php
@@ -23,20 +23,4 @@ use WellCommerce\Bundle\CoreBundle\DependencyInjection\AbstractExtension;
  */
 class WellCommerceAppExtension extends AbstractExtension
 {
-//    /**
-//     * {@inheritdoc}
-//     */
-//    public function load(array $configs, ContainerBuilder $container)
-//    {
-//        parent::load($configs, $container);
-//
-//        $configuration = $this->getConfiguration($configs, $container);
-//        $config        = $this->processConfiguration($configuration, $configs);
-//        $type          = $config['type'];
-//
-//        $container->setParameter('search_term_min_length', $config['search_term_min_length']);
-//        $container->setAlias('product_search.indexer', sprintf('product_search.indexer.%s', $type));
-//        $container->setAlias('search_index.manager', sprintf('search_index.manager.lucene', $type));
-//        $container->setAlias('product_search.provider', sprintf('product_search.provider.lucene', $type));
-//    }
 }
