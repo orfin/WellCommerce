a95ee258bb18a4d0ca666638917053b7d37461b1
Adam Piotrowski <adam@wellcommerce.org>
2015-12-08 02:18:55 +0100

    [STree] Added stree 'routingbundle' in src/WellCommerce/Bundle/RoutingBundle

---
+++ src/WellCommerce/Bundle/RoutingBundle/DependencyInjection/WellCommerceRoutingExtension.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\RoutingBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader;
+use WellCommerce\Bundle\CoreBundle\DependencyInjection\AbstractExtension;
+
+/**
+ * Class WellCommerceRoutingExtension
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class WellCommerceRoutingExtension extends AbstractExtension
+{
+
+}
