b432cd3bcbc6358b411ac78368bd71aee0845442
Adam Piotrowski <adam@wellcommerce.org>
2015-12-08 12:34:45 +0100

    [STree] Added stree 'shippingbundle' in src/WellCommerce/Bundle/ShippingBundle

---
+++ src/WellCommerce/Bundle/ShippingBundle/WellCommerceShippingBundle.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\ShippingBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use WellCommerce\Bundle\ShippingBundle\DependencyInjection\Compiler\RegisterShippingMethodCalculatorPass;
+
+/**
+ * Class WellCommerceShippingBundle
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class WellCommerceShippingBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new RegisterShippingMethodCalculatorPass());
+    }
+}
