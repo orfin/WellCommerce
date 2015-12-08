f1e363eb2acc35347c6bb08b9c4da1ac9a33e1ca
Adam Piotrowski <adam@wellcommerce.org>
2015-12-08 11:54:01 +0100

    Removed pull option from sync script

--- gitsync.sh
+++ gitsync.sh
@@ -138,51 +138,6 @@ if [ "$action" == 'push' ]
         git stree push WishlistBundle
 fi
 
-if [ "$action" == 'pull' ]
-    then
-        git stree push AdminBundle
-        git stree push AppBundle
-        git stree push AttributeBundle
-        git stree push AvailabilityBundle
-        git stree push CartBundle
-        git stree push CategoryBundle
-        git stree push ClientBundle
-        git stree push Collections
-        git stree push CompanyBundle
-        git stree push ContactBundle
-        git stree push CoreBundle
-        git stree push CountryBundle
-        git stree push CouponBundle
-        git stree push CurrencyBundle
-        git stree push DataGrid
-        git stree push DataSet
-        git stree push DelivererBundle
-        git stree push DictionaryBundle
-        git stree push Form
-        git stree push LayeredNavigationBundle
-        git stree push LayoutBundle
-        git stree push LocaleBundle
-        git stree push MediaBundle
-        git stree push NewsBundle
-        git stree push OrderBundle
-        git stree push PageBundle
-        git stree push PaymentBundle
-        git stree push ProducerBundle
-        git stree push ProductBundle
-        git stree push ProductStatusBundle
-        git stree push ReportBundle
-        git stree push ReviewBundle
-        git stree push RoutingBundle
-        git stree push SearchBundle
-        git stree push ShippingBundle
-        git stree push ShopBundle
-        git stree push SmugglerBundle
-        git stree push TaxBundle
-        git stree push ThemeBundle
-        git stree push UnitBundle
-        git stree push WishlistBundle
-fi
-
 if [ "$action" == 'init' ]
     then
         git stree add Collections -P src/WellCommerce/Component/Collections git@github.com:WellCommerce/Collections.git
