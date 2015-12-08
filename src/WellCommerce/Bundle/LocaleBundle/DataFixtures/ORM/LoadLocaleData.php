c09f64c3e6cdd2d0e17614991cd975f5e5ff20e8
Adam Piotrowski <adam@wellcommerce.org>
2015-12-08 02:18:16 +0100

    [STree] Added stree 'localebundle' in src/WellCommerce/Bundle/LocaleBundle

---
+++ src/WellCommerce/Bundle/LocaleBundle/DataFixtures/ORM/LoadLocaleData.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\LocaleBundle\DataFixtures\ORM;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use WellCommerce\Bundle\CoreBundle\DataFixtures\AbstractDataFixture;
+use WellCommerce\Bundle\LocaleBundle\Entity\Locale;
+
+/**
+ * Class LoadLocaleData
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class LoadLocaleData extends AbstractDataFixture
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $en = new Locale();
+        $en->setCode('en');
+        $en->setCurrency($this->getReference('currency_USD'));
+        $manager->persist($en);
+        $manager->flush();
+
+        $this->setReference('locale_en', $en);
+    }
+}
