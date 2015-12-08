14755ac34f5d0f6e78ae01e83966327ab46ab1e4
Adam Piotrowski <adam@wellcommerce.org>
2015-12-08 12:34:15 +0100

    [STree] Added stree 'orderbundle' in src/WellCommerce/Bundle/OrderBundle

---
+++ src/WellCommerce/Bundle/OrderBundle/Entity/OrderStatusGroupInterface.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\OrderBundle\Entity;
+
+use WellCommerce\Bundle\CoreBundle\Entity\BlameableInterface;
+use WellCommerce\Bundle\CoreBundle\Entity\TimestampableInterface;
+use WellCommerce\Bundle\CoreBundle\Entity\TranslatableInterface;
+
+/**
+ * Interface OrderStatusGroupInterface
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+interface OrderStatusGroupInterface extends TranslatableInterface, TimestampableInterface, BlameableInterface
+{
+    /**
+     * @return integer
+     */
+    public function getId();
+}
