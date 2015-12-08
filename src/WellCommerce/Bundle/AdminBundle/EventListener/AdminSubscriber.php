6895be86a0ea3f75a5e566e796ffb73e95490e1d
Adam Piotrowski <adam@wellcommerce.org>
2015-12-08 02:17:22 +0100

    [STree] Added stree 'adminbundle' in src/WellCommerce/Bundle/AdminBundle

---
+++ src/WellCommerce/Bundle/AdminBundle/EventListener/AdminSubscriber.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+namespace WellCommerce\Bundle\AdminBundle\EventListener;
+
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use WellCommerce\Bundle\AdminBundle\Entity\UserInterface;
+use WellCommerce\Bundle\CoreBundle\EventListener\AbstractEventSubscriber;
+
+/**
+ * Class AdminSubscriber
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class AdminSubscriber extends AbstractEventSubscriber
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::CONTROLLER => ['onKernelController', 0],
+        ];
+    }
+
+    public function onKernelController(FilterControllerEvent $event)
+    {
+        $user = $this->getUser();
+        if ($user instanceof UserInterface) {
+            $route = $this->getRouterHelper()->getCurrentRoute();
+            if ($route->hasOption('require_admin_permission')) {
+                $name       = $route->getOption('require_admin_permission');
+                $permission = $this->getSecurityHelper()->getPermission($name, $user);
+                if (empty($permission)) {
+                    $event->setController([$this->get('user.controller.admin'), 'accessDeniedAction']);
+                }
+            }
+        }
+    }
+}
