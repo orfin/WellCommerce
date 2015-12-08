913c190c6f4b442e68b93aa25d9283f2a9c9d29b
Adam Piotrowski <adam@wellcommerce.org>
2015-12-08 12:34:21 +0100

    [STree] Added stree 'productbundle' in src/WellCommerce/Bundle/ProductBundle

---
+++ src/WellCommerce/Bundle/ProductBundle/Factory/ProductFactory.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\ProductBundle\Factory;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use WellCommerce\Bundle\CoreBundle\Factory\AbstractFactory;
+use WellCommerce\Bundle\ProductBundle\Entity\Product;
+
+/**
+ * Class ProductFactory
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class ProductFactory extends AbstractFactory
+{
+    /**
+     * @return \WellCommerce\Bundle\ProductBundle\Entity\ProductInterface
+     */
+    public function create()
+    {
+        $product = new Product();
+        $product->setCategories(new ArrayCollection());
+        $product->setProductPhotos(new ArrayCollection());
+        $product->setStatuses(new ArrayCollection());
+        $product->setAttributes(new ArrayCollection());
+        $product->setShops(new ArrayCollection());
+        $product->setEnabled(true);
+
+        return $product;
+    }
+}
