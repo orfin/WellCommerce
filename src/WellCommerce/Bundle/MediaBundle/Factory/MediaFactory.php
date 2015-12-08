24e28e7d787593fca196a2ad611a707a81dfa0c6
Adam Piotrowski <adam@wellcommerce.org>
2015-12-08 12:34:06 +0100

    [STree] Added stree 'mediabundle' in src/WellCommerce/Bundle/MediaBundle

---
+++ src/WellCommerce/Bundle/MediaBundle/Factory/MediaFactory.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\MediaBundle\Factory;
+
+use WellCommerce\Bundle\CoreBundle\Factory\AbstractFactory;
+use WellCommerce\Bundle\MediaBundle\Entity\Media;
+
+/**
+ * Class MediaFactory
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class MediaFactory extends AbstractFactory
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function create()
+    {
+        $media = new Media();
+
+        return $media;
+    }
+}
