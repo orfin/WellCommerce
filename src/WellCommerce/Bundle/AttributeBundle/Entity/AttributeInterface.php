d5ca497df5327d411a3ab0bb3b7add82f70aac56
Adam Piotrowski <adam@wellcommerce.org>
2015-12-08 02:17:27 +0100

    [STree] Added stree 'attributebundle' in src/WellCommerce/Bundle/AttributeBundle

---
+++ src/WellCommerce/Bundle/AttributeBundle/Entity/AttributeInterface.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\AttributeBundle\Entity;
+
+use Doctrine\Common\Collections\Collection;
+use WellCommerce\Bundle\AttributeBundle\Entity\Attribute\GroupInterface;
+use WellCommerce\Bundle\CoreBundle\Entity\BlameableInterface;
+use WellCommerce\Bundle\CoreBundle\Entity\TimestampableInterface;
+use WellCommerce\Bundle\CoreBundle\Entity\TranslatableInterface;
+
+/**
+ * Interface AttributeInterface
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+interface AttributeInterface extends TimestampableInterface, TranslatableInterface, BlameableInterface
+{
+    /**
+     * @return int
+     */
+    public function getId();
+
+    /**
+     * @return GroupInterface
+     */
+    public function getAttributeGroup();
+
+    /**
+     * @param GroupInterface $attributeGroup
+     */
+    public function setAttributeGroup(GroupInterface $attributeGroup);
+
+    /**
+     * @return Collection
+     */
+    public function getValues();
+
+    /**
+     * @param Collection $collection
+     */
+    public function setValues(Collection $collection);
+
+    /**
+     * @param AttributeValueInterface $value
+     */
+    public function removeValue(AttributeValueInterface $value);
+}
