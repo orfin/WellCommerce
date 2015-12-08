6d4f534cc0bcf3073fc3086c640b8c19efd35cca
Adam Piotrowski <adam@wellcommerce.org>
2015-12-08 12:34:09 +0100

    [STree] Added stree 'newsbundle' in src/WellCommerce/Bundle/NewsBundle

---
+++ src/WellCommerce/Bundle/NewsBundle/Factory/NewsFactory.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\NewsBundle\Factory;
+
+use WellCommerce\Bundle\CoreBundle\Factory\AbstractFactory;
+use WellCommerce\Bundle\NewsBundle\Entity\News;
+
+class NewsFactory extends AbstractFactory
+{
+    /**
+     * @return \WellCommerce\Bundle\NewsBundle\Entity\NewsInterface
+     */
+    public function create()
+    {
+        $news = new News();
+        $news->setFeatured(false);
+        $news->setPublish(true);
+        $news->setStartDate(new \DateTime());
+
+        return $news;
+    }
+}
