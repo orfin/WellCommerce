3a60a67d89ec71b3bfb01a287272b1cef8b9af56
Adam Piotrowski <adam@wellcommerce.org>
2015-12-09 13:13:46 +0100

    [STree] Added stree 'dictionarybundle' in src/WellCommerce/Bundle/DictionaryBundle

---
+++ src/WellCommerce/Bundle/DictionaryBundle/Tests/DependencyInjection/DictionaryExtensionTest.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\DictionaryBundle\Tests\DependencyInjection;
+
+use WellCommerce\Bundle\CoreBundle\Test\DependencyInjection\AbstractExtensionTestCase;
+
+/**
+ * Class DictionaryExtensionTest
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class DictionaryExtensionTest extends AbstractExtensionTestCase
+{
+    /**
+     * @return array
+     */
+    public function getRequiredServices()
+    {
+        return [
+            'services' => [
+                [
+                    'dictionary.repository',
+                    'dictionary.factory',
+                    'dictionary.event_dispatcher',
+                    'dictionary.form_builder.admin',
+                    'dictionary.dataset.admin',
+                    'dictionary.datagrid',
+                    'dictionary.controller.admin',
+                ]
+            ],
+        ];
+    }
+}
