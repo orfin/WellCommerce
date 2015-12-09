0937ec800505a993c5eefacc658078208fdc40b3
Adam Piotrowski <adam@wellcommerce.org>
2015-12-09 13:14:46 +0100

    [STree] Added stree 'taxbundle' in src/WellCommerce/Bundle/TaxBundle

---
+++ src/WellCommerce/Bundle/TaxBundle/Factory/TaxFactory.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\TaxBundle\Factory;
+
+use WellCommerce\Bundle\CoreBundle\Factory\AbstractFactory;
+use WellCommerce\Bundle\TaxBundle\Entity\Tax;
+
+/**
+ * Class TaxFactory
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class TaxFactory extends AbstractFactory
+{
+    /**
+     * @return \WellCommerce\Bundle\TaxBundle\Entity\TaxInterface
+     */
+    public function create()
+    {
+        $tax = new Tax();
+        $tax->setValue(0);
+        $tax->setCreatedAt(new \DateTime());
+
+        return $tax;
+    }
+}
