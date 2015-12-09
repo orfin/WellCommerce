4e90f82ff6ee22d2771d3b45b6b86b3c171b3bde
Adam Piotrowski <adam@wellcommerce.org>
2015-12-09 13:13:53 +0100

    [STree] Added stree 'layoutbundle' in src/WellCommerce/Bundle/LayoutBundle

---
+++ src/WellCommerce/Bundle/LayoutBundle/Repository/LayoutBoxRepository.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+namespace WellCommerce\Bundle\LayoutBundle\Repository;
+
+use WellCommerce\Bundle\CoreBundle\Repository\AbstractEntityRepository;
+use WellCommerce\Bundle\LayoutBundle\Collection\LayoutBoxCollection;
+
+/**
+ * Class LayoutBoxRepository
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class LayoutBoxRepository extends AbstractEntityRepository implements LayoutBoxRepositoryInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getLayoutBoxesCollection()
+    {
+        $boxes = $this->findAll();
+
+        return new LayoutBoxCollection($boxes);
+    }
+}
