00787764d0aff871af43e930582c8b1ff37ef1c0
Adam Piotrowski <adam@wellcommerce.org>
2015-12-09 13:14:11 +0100

    [STree] Added stree 'paymentbundle' in src/WellCommerce/Bundle/PaymentBundle

---
+++ src/WellCommerce/Bundle/PaymentBundle/Factory/PaymentMethodFactory.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\PaymentBundle\Factory;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use WellCommerce\Bundle\CoreBundle\Factory\AbstractFactory;
+use WellCommerce\Bundle\PaymentBundle\Entity\PaymentMethod;
+
+/**
+ * Class PaymentMethodFactory
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class PaymentMethodFactory extends AbstractFactory
+{
+    /**
+     * @return \WellCommerce\Bundle\PaymentBundle\Entity\PaymentMethodInterface
+     */
+    public function create()
+    {
+        $paymentMethod = new PaymentMethod();
+        $paymentMethod->setHierarchy(0);
+        $paymentMethod->setEnabled(true);
+        $paymentMethod->setConfiguration(new ArrayCollection());
+        $paymentMethod->setShippingMethods(new ArrayCollection());
+
+        return $paymentMethod;
+    }
+}
