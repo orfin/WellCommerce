8cfa29445a6aecdece3d88345c34ad741abbc6a9
Adam Piotrowski <adam@wellcommerce.org>
2015-12-09 13:15:23 +0100

    The type hint of parameter "producer" in method "addProducer"
    (cherry picked from commit 7543493916a5e1cdd33ab8d9b3021c014302b942)

--- Entity/Deliverer.php
+++ Entity/Deliverer.php
@@ -16,6 +16,7 @@ use Doctrine\Common\Collections\Collection;
 use Knp\DoctrineBehaviors\Model\Blameable\Blameable;
 use Knp\DoctrineBehaviors\Model\Timestampable\Timestampable;
 use Knp\DoctrineBehaviors\Model\Translatable\Translatable;
+use WellCommerce\Bundle\ProducerBundle\Entity\ProducerInterface;
 
 /**
  * Class Deliverer
