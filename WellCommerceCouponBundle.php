dca3a152780d116b2f94ff9041b371801f93c4b9
Adam Piotrowski <adam@wellcommerce.org>
2015-12-12 00:00:41 +0100

    Added compiler passes to all bundles
    (cherry picked from commit 980ebd68a7aa0b9c6be3c64f30875a7ce00bc22f)

--- WellCommerceCouponBundle.php
+++ WellCommerceCouponBundle.php
@@ -12,7 +12,9 @@
 
 namespace WellCommerce\Bundle\CouponBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use WellCommerce\Bundle\CouponBundle\DependencyInjection\Compiler;
 
 /**
  * Class WellCommerceCouponBundle
@@ -21,4 +23,10 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class WellCommerceCouponBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new Compiler\AutoRegisterServicesPass());
+        $container->addCompilerPass(new Compiler\MappingCompilerPass());
+    }
 }
