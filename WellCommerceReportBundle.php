5cc2cd7ab9b043e7337b584c8e6c7f6b426a625e
Adam Piotrowski <adam@wellcommerce.org>
2015-12-12 00:00:41 +0100

    Added compiler passes to all bundles
    (cherry picked from commit 980ebd68a7aa0b9c6be3c64f30875a7ce00bc22f)

--- WellCommerceReportBundle.php
+++ WellCommerceReportBundle.php
@@ -12,7 +12,9 @@
 
 namespace WellCommerce\Bundle\ReportBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use WellCommerce\Bundle\ReportBundle\DependencyInjection\Compiler;
 
 /**
  * Class WellCommerceReportBundle
@@ -21,4 +23,10 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class WellCommerceReportBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new Compiler\AutoRegisterServicesPass());
+        $container->addCompilerPass(new Compiler\MappingCompilerPass());
+    }
 }
