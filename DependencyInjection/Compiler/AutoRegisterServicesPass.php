02cf90d5a45d1e3d18c7271b595eb48df3291f66
Adam Piotrowski <adam@wellcommerce.org>
2015-12-11 23:25:38 +0100

    Modified compiler passes to reflect auto-service changes
    (cherry picked from commit 876fc545c62a7d5b9fe70ef2e5fcf937073d9a88)

--- DependencyInjection/Compiler/AutoRegisterServicesPass.php
+++ DependencyInjection/Compiler/AutoRegisterServicesPass.php
@@ -15,6 +15,7 @@ namespace WellCommerce\Bundle\TaxBundle\DependencyInjection\Compiler;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use WellCommerce\Bundle\AttributeBundle\DependencyInjection\WellCommerceAttributeExtension;
 use WellCommerce\Bundle\CoreBundle\DependencyInjection\Compiler\AbstractAutoRegisterServicesPass;
+use WellCommerce\Bundle\TaxBundle\DependencyInjection\WellCommerceTaxExtension;
 
 /**
  * Class MappingCompilerPass
@@ -25,6 +26,6 @@ class AutoRegisterServicesPass extends AbstractAutoRegisterServicesPass
 {
     protected function getExtensionConfiguration(ContainerBuilder $container)
     {
-        return $container->getParameter(WellCommerceAttributeExtension::EXTENSION_NAME);
+        return $container->getParameter(WellCommerceTaxExtension::EXTENSION_NAME);
     }
 }
