6b3583c396bb1e9c576f3fb3d3595857833449b9
Adam Piotrowski <adam@wellcommerce.org>
2015-12-15 15:31:04 +0100

    [STree] Added stree 'pagebundle' in src/WellCommerce/Bundle/PageBundle

---
+++ src/WellCommerce/Bundle/PageBundle/DependencyInjection/WellCommercePageExtension.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\PageBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
+use WellCommerce\Bundle\CoreBundle\DependencyInjection\AbstractExtension;
+
+/**
+ * Class WellCommercePageExtension
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class WellCommercePageExtension extends AbstractExtension
+{
+    CONST EXTENSION_NAME = 'well_commerce_page';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setExtensionConfiguration(ContainerBuilder $container, array $parameters = [])
+    {
+        $container->setParameter(self::EXTENSION_NAME, $parameters);
+    }
+}
