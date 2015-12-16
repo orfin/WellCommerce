fcde4a81e75a35cf60ddb522aefdccc8567647f1
Adam Piotrowski <adam@wellcommerce.org>
2015-12-16 23:28:41 +0100

    [STree] Added stree 'clientbundle' in src/WellCommerce/Bundle/ClientBundle

---
+++ src/WellCommerce/Bundle/ClientBundle/Entity/ClientContactDetails.php
@@ -0,0 +1,128 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\ClientBundle\Entity;
+
+use WellCommerce\Bundle\ClientBundle\Entity\ContactDetails;
+
+/**
+ * Class ClientContactDetails
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class ClientContactDetails implements ClientContactDetailsInterface
+{
+    /**
+     * @var string
+     */
+    protected $firstName;
+
+    /**
+     * @var string
+     */
+    protected $lastName;
+
+    /**
+     * @var string
+     */
+    protected $phone;
+
+    /**
+     * @var string
+     */
+    protected $secondaryPhone;
+
+    /**
+     * @var string
+     */
+    protected $email;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSecondaryPhone()
+    {
+        return $this->secondaryPhone;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setSecondaryPhone($secondaryPhone)
+    {
+        $this->secondaryPhone = $secondaryPhone;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+}
---
+++ src/WellCommerce/Bundle/ClientBundle/Entity/ClientDetailsInterface.php
@@ -0,0 +1,81 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\ClientBundle\Entity;
+
+/**
+ * Interface ClientContactDetailsInterface
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+interface ClientDetailsInterface
+{
+    /**
+     * @return float
+     */
+    public function getDiscount();
+
+    /**
+     * @param float $discount
+     */
+    public function setDiscount($discount);
+
+    /**
+     * @param string $password
+     */
+    public function setPassword($password);
+
+    /**
+     * @return void
+     */
+    public function resetPassword();
+
+    /**
+     * @param string $username
+     */
+    public function setUsername($username);
+
+    /**
+     * @param string $salt
+     */
+    public function setSalt($salt);
+
+    /**
+     * @return bool
+     */
+    public function isConditionsAccepted();
+
+    /**
+     * @param bool $conditionsAccepted
+     */
+    public function setConditionsAccepted($conditionsAccepted);
+
+    /**
+     * @return bool
+     */
+    public function isNewsletterAccepted();
+
+    /**
+     * @param bool $newsletterAccepted
+     */
+    public function setNewsletterAccepted($newsletterAccepted);
+
+    /**
+     * @return string
+     */
+    public function getResetPasswordHash();
+
+    /**
+     * @param string $resetPasswordHash
+     */
+    public function setResetPasswordHash($resetPasswordHash);
+}
