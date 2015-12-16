989ae6324b35a2ea1fe2b8872dee2ac623669693
Adam Piotrowski <adam@wellcommerce.org>
2015-12-16 23:28:24 +0100

    [STree] Added stree 'availabilitybundle' in src/WellCommerce/Bundle/AvailabilityBundle

---
+++ src/WellCommerce/Bundle/AvailabilityBundle/Form/Admin/AvailabilityFormBuilder.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+namespace WellCommerce\Bundle\AvailabilityBundle\Form\Admin;
+
+use WellCommerce\Bundle\CoreBundle\Form\AbstractFormBuilder;
+use WellCommerce\Component\Form\Elements\FormInterface;
+
+/**
+ * Class AvailabilityForm
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class AvailabilityFormBuilder extends AbstractFormBuilder
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormInterface $form)
+    {
+        $requiredData = $form->addChild($this->getElement('nested_fieldset', [
+            'name'  => 'general_settings',
+            'label' => $this->trans('common.fieldset.general')
+        ]));
+
+        $languageData = $requiredData->addChild($this->getElement('language_fieldset', [
+            'name'        => 'translations',
+            'label'       => $this->trans('form.fieldset.translations'),
+            'transformer' => $this->getRepositoryTransformer('translation', $this->get('availability.repository'))
+        ]));
+
+        $languageData->addChild($this->getElement('text_field', [
+            'name'  => 'name',
+            'label' => $this->trans('common.label.name'),
+        ]));
+
+        $form->addFilter($this->getFilter('no_code'));
+        $form->addFilter($this->getFilter('trim'));
+        $form->addFilter($this->getFilter('secure'));
+    }
+}
