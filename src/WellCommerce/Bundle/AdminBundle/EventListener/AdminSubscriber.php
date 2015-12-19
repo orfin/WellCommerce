5a9ae4d765bb291133cb2ff0a9808639fbb5361e
Adam Piotrowski <adam@wellcommerce.org>
2015-12-19 16:26:03 +0100

    [STree] Added stree 'adminbundle' in src/WellCommerce/Bundle/AdminBundle

---
+++ src/WellCommerce/Bundle/AdminBundle/EventListener/AdminSubscriber.php
@@ -0,0 +1,76 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+namespace WellCommerce\Bundle\AdminBundle\EventListener;
+
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use WellCommerce\Bundle\AdminBundle\Entity\UserInterface;
+use WellCommerce\Bundle\CoreBundle\Event\ResourceEvent;
+use WellCommerce\Bundle\CoreBundle\EventListener\AbstractEventSubscriber;
+
+/**
+ * Class AdminSubscriber
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class AdminSubscriber extends AbstractEventSubscriber
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::CONTROLLER => ['onKernelController', 0],
+            'user.pre_create'        => ['onUserPreCreate', 0],
+        ];
+    }
+
+    public function onKernelController(FilterControllerEvent $event)
+    {
+        $user = $this->getUser();
+        if ($user instanceof UserInterface) {
+            $route = $this->getRouterHelper()->getCurrentRoute();
+            if ($route->hasOption('require_admin_permission')) {
+                $name       = $route->getOption('require_admin_permission');
+                $permission = $this->getSecurityHelper()->getPermission($name, $user);
+                if (empty($permission)) {
+                    $event->setController([$this->get('user.controller.admin'), 'accessDeniedAction']);
+                }
+            }
+        }
+    }
+
+    public function onUserPreCreate(ResourceEvent $resourceEvent)
+    {
+        $password = $this->getSecurityHelper()->generateRandomPassword();
+        $role     = $this->get('role.repository')->findOneByName('admin');
+        $user     = $resourceEvent->getResource();
+        if ($user instanceof UserInterface) {
+            $user->addRole($role);
+            $user->setPassword($password);
+
+            $email = $user->getEmail();
+            $title = $this->getTranslatorHelper()->trans('user.email.title.register');
+            $body  = $this->getEmailBody($user, $password);
+            $shop  = $this->get('shop.context.admin')->getCurrentShop();
+
+            $this->getMailerHelper()->sendEmail($email, $title, $body, $shop->getMailerConfiguration());
+        }
+    }
+
+    protected function getEmailBody(UserInterface $user, $password)
+    {
+        return $this->getTemplatingelper()->render(
+            'WellCommerceAdminBundle:Admin/Email:register.html.twig', [
+            'user'     => $user,
+            'password' => $password
+        ]);
+    }
+}
