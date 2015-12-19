7c4a09032df35455595981bad5ef3128266a331e
Adam Piotrowski <adam@wellcommerce.org>
2015-12-19 16:17:34 +0100

    [STree] Added stree 'attributebundle' in src/WellCommerce/Bundle/AttributeBundle

---
+++ src/WellCommerce/Bundle/AttributeBundle/Repository/AttributeRepositoryInterface.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\AttributeBundle\Repository;
+
+use WellCommerce\Bundle\AttributeBundle\Entity\AttributeGroupInterface;
+use WellCommerce\Bundle\CoreBundle\Repository\RepositoryInterface;
+
+/**
+ * Interface AttributeRepositoryInterface
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+interface AttributeRepositoryInterface extends RepositoryInterface
+{
+    /**
+     * Returns all attributes for group
+     *
+     * @param AttributeGroupInterface $attributeGroup
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getCollectionByAttributeGroup(AttributeGroupInterface $attributeGroup);
+}
