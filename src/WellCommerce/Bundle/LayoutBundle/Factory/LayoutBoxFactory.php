f56e2be5cf4fa59ea9915860528426148aaee02a
Adam Piotrowski <adam@wellcommerce.org>
2015-12-19 16:26:51 +0100

    [STree] Added stree 'layoutbundle' in src/WellCommerce/Bundle/LayoutBundle

---
+++ src/WellCommerce/Bundle/LayoutBundle/Factory/LayoutBoxFactory.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\LayoutBundle\Factory;
+
+use WellCommerce\Bundle\CoreBundle\Factory\AbstractFactory;
+use WellCommerce\Bundle\LayoutBundle\Entity\LayoutBoxInterface;
+
+/**
+ * Class LayoutBoxFactory
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class LayoutBoxFactory extends AbstractFactory
+{
+    /**
+     * @var string
+     */
+    protected $supportsInterface = LayoutBoxInterface::class;
+
+    /**
+     * @return LayoutBoxInterface
+     */
+    public function create()
+    {
+        /** @var $box LayoutBoxInterface */
+        $box = $this->init();
+        $box->setIdentifier('');
+
+        return $box;
+    }
+}
