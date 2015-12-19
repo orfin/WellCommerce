e47583134e601c86b1c02b5edc6bee21e89baba9
Adam Piotrowski <adam@wellcommerce.org>
2015-12-19 16:26:41 +0100

    [STree] Added stree 'delivererbundle' in src/WellCommerce/Bundle/DelivererBundle

---
+++ src/WellCommerce/Bundle/DelivererBundle/DataFixtures/ORM/LoadDelivererData.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\DelivererBundle\DataFixtures\ORM;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use WellCommerce\Bundle\CoreBundle\DataFixtures\AbstractDataFixture;
+use WellCommerce\Bundle\DelivererBundle\Entity\Deliverer;
+
+/**
+ * Class LoadDelivererData
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class LoadDelivererData extends AbstractDataFixture
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        if (!$this->isEnabled()) {
+            return;
+        }
+
+        $fakerGenerator = $this->getFakerGenerator();
+        $deliverer      = new Deliverer();
+        $name           = $fakerGenerator->company;
+        $deliverer->translate('en')->setName($name);
+        $deliverer->mergeNewTranslations();
+        $manager->persist($deliverer);
+        $manager->flush();
+
+        $this->setReference('deliverer', $deliverer);
+    }
+}
