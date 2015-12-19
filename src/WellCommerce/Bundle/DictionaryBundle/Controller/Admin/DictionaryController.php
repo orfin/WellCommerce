97a2405900dc2c5bb56807f52c201917e7fb6fee
Adam Piotrowski <adam@wellcommerce.org>
2015-12-19 16:18:09 +0100

    [STree] Added stree 'dictionarybundle' in src/WellCommerce/Bundle/DictionaryBundle

---
+++ src/WellCommerce/Bundle/DictionaryBundle/Controller/Admin/DictionaryController.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\DictionaryBundle\Controller\Admin;
+
+use Symfony\Component\HttpFoundation\Request;
+use WellCommerce\Bundle\CoreBundle\Controller\Admin\AbstractAdminController;
+
+/**
+ * Class DictionaryController
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class DictionaryController extends AbstractAdminController
+{
+    /**
+     * @var \WellCommerce\Bundle\DictionaryBundle\Manager\Admin\DictionaryManager
+     */
+    protected $manager;
+
+    /**
+     * Synchronizes translations to filesystem and database
+     *
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function syncAction(Request $request)
+    {
+        $this->manager->syncDictionary($request, $this->get('kernel'));
+        $this->manager->getFlashHelper()->addSuccess('translation.flashes.success.synchronization');
+
+        return $this->redirectToAction('index');
+    }
+}
