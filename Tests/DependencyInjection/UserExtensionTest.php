aa9d7e9503abde7344e764617802faa3d5921662
Adam Piotrowski <adam@wellcommerce.org>
2015-12-19 16:06:07 +0100

    User tests
    (cherry picked from commit caa6d87011d03cd263659ad8d496b33f3d85ad5d)

---
+++ Tests/DependencyInjection/UserExtensionTest.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\AdminBundle\Tests\DependencyInjection;
+
+use WellCommerce\Bundle\CoreBundle\Test\DependencyInjection\AbstractExtensionTestCase;
+
+/**
+ * Class UserExtensionTest
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class UserExtensionTest extends AbstractExtensionTestCase
+{
+    /**
+     * @return array
+     */
+    public function getRequiredServices()
+    {
+        return [
+            'services' => [
+                [
+                    'user.repository',
+                    'user.factory',
+                    'user.event_dispatcher',
+                    'user.form_builder.admin',
+                    'user.dataset.admin',
+                    'user.datagrid',
+                    'user.controller.admin',
+                ]
+            ],
+        ];
+    }
+}
