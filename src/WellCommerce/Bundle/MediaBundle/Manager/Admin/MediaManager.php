642bc8db69f24e41d66f9cf9924a50d6b64f8df3
Adam Piotrowski <adam@wellcommerce.org>
2015-12-19 16:18:20 +0100

    [STree] Added stree 'mediabundle' in src/WellCommerce/Bundle/MediaBundle

---
+++ src/WellCommerce/Bundle/MediaBundle/Manager/Admin/MediaManager.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\MediaBundle\Manager\Admin;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use WellCommerce\Bundle\CoreBundle\Manager\Admin\AbstractAdminManager;
+
+/**
+ * Class MediaManager
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class MediaManager extends AbstractAdminManager
+{
+    /**
+     * Uploads the file
+     *
+     * @param UploadedFile $file
+     * @param              $dir
+     *
+     * @return \WellCommerce\Bundle\MediaBundle\Entity\MediaInterface
+     * @throws \Exception
+     */
+    public function upload(UploadedFile $file, $dir)
+    {
+        $uploadPath = $this->getUploadRootDir($dir);
+
+        if (!$file->isValid()) {
+            throw new \Exception('Passed file object is not valid');
+        }
+
+        $media = $this->createMediaFromUploadedFile($file);
+        $this->saveResource($media);
+
+        $file->move($uploadPath, $media->getPath());
+
+        return $media;
+    }
+
+    /**
+     * @param UploadedFile $file
+     *
+     * @return \WellCommerce\Bundle\MediaBundle\Entity\MediaInterface
+     */
+    protected function createMediaFromUploadedFile(UploadedFile $file)
+    {
+        $media = $this->initResource();
+        $media->setName($file->getClientOriginalName());
+        $media->setExtension($file->guessClientExtension());
+        $media->setMime($file->getClientMimeType());
+        $media->setSize($file->getClientSize());
+
+        return $media;
+    }
+
+    /**
+     * Returns upload directory
+     *
+     * @param string $dir
+     *
+     * @return string
+     */
+    public function getUploadRootDir($dir)
+    {
+        $rootDir = $this->getKernel()->getRootDir();
+        $dir     = rtrim($dir, '/\\');
+
+        return sprintf('%s/../web/media/%s', $rootDir, $dir);
+    }
+}
