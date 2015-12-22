005035c6f473c03201c4da786d13a07243b36374
Adam Piotrowski <adam@wellcommerce.org>
2015-12-22 20:11:08 +0100

    [STree] Added stree 'layerednavigationbundle' in src/WellCommerce/Bundle/LayeredNavigationBundle

---
+++ src/WellCommerce/Bundle/LayeredNavigationBundle/WellCommerceLayeredNavigationBundle.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\LayeredNavigationBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use WellCommerce\Bundle\LayeredNavigationBundle\DependencyInjection\Compiler;
+
+/**
+ * Class WellCommerceLayeredNavigationBundleBundle
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class WellCommerceLayeredNavigationBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
+}
