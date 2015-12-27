7a1810785575686ffeb7bed942bb07468e216772
Adam Piotrowski <adam@wellcommerce.org>
2015-12-27 21:46:37 +0100

    Added install command

--- rebuild.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-env=$1
-
-/usr/local/php5/bin/php app/console doctrine:fixtures:load --env=$env --no-interaction
-/usr/local/php5/bin/php app/console assets:install --env=$env
-/usr/local/php5/bin/php app/console bazinga:js-translation:dump --env=$env
-/usr/local/php5/bin/php app/console fos:js-routing:dump --env=$env
-/usr/local/php5/bin/php app/console assetic:dump --env=$env
-/usr/local/php5/bin/php app/console cache:warmup --env=$env
