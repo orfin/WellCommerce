8ca6a772d30c7cd78964058ec77b02e058e4b8f4
Adam Piotrowski <adam@wellcommerce.org>
2015-12-28 23:58:50 +0100

    Added test alias method to repository tests
    (cherry picked from commit 083c7ea76150f8dfa3ca198e696efbdaaf5f0b8d)

--- Tests/Repository/UserRepositoryTest.php
+++ Tests/Repository/UserRepositoryTest.php
@@ -21,6 +21,11 @@ use WellCommerce\Bundle\CoreBundle\Test\Repository\AbstractRepositoryTestCase;
  */
 class UserRepositoryTest extends AbstractRepositoryTestCase
 {
+    protected function getAlias()
+    {
+        return 'user';
+    }
+
     protected function get()
     {
         return $this->container->get('user.repository');
