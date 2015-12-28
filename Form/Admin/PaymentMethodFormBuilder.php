4a7cd5f839bdafdb5bdb3ca5d65e69dfb3c2860d
Adam Piotrowski <adam@wellcommerce.org>
2015-12-28 15:39:49 +0100

    Code reformatted
    (cherry picked from commit 1af32266582008b39c84a5988deaa052bca1d4a0)

--- Form/Admin/PaymentMethodFormBuilder.php
+++ Form/Admin/PaymentMethodFormBuilder.php
@@ -60,13 +60,13 @@ class PaymentMethodFormBuilder extends AbstractFormBuilder
         ]));
 
         $requiredData->addChild($this->getElement('checkbox', [
-            'name'    => 'enabled',
-            'label'   => $this->trans('common.label.enabled'),
+            'name'  => 'enabled',
+            'label' => $this->trans('common.label.enabled'),
         ]));
 
         $requiredData->addChild($this->getElement('text_field', [
-            'name'    => 'hierarchy',
-            'label'   => $this->trans('common.label.hierarchy'),
+            'name'  => 'hierarchy',
+            'label' => $this->trans('common.label.hierarchy'),
             'rules' => [
                 $this->getRule('required')
             ],
