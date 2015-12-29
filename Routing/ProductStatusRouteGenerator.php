9571b2c35c6165aeabf840b0ee257f9c67104c13
Adam Piotrowski <adam@wellcommerce.org>
2015-12-29 01:07:25 +0100

    Changed "Generator" to "Routing"
    (cherry picked from commit c37415c609f05445cb6ff17aa5321ccf718b076a)

--- Routing/ProductStatusRouteGenerator.php
+++ Routing/ProductStatusRouteGenerator.php
@@ -10,7 +10,7 @@
  * please view the LICENSE file that was distributed with this source code.
  */
 
-namespace WellCommerce\Bundle\ProductStatusBundle\Generator;
+namespace WellCommerce\Bundle\ProductStatusBundle\Routing;
 
 use WellCommerce\Bundle\RoutingBundle\Generator\AbstractRouteGenerator;
 
