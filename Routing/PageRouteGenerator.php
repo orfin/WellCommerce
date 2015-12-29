2cbc2794e0ff45a6564e2da33de87a8d1c8a6de0
Adam Piotrowski <adam@wellcommerce.org>
2015-12-29 01:07:25 +0100

    Changed "Generator" to "Routing"
    (cherry picked from commit c37415c609f05445cb6ff17aa5321ccf718b076a)

--- Routing/PageRouteGenerator.php
+++ Routing/PageRouteGenerator.php
@@ -10,7 +10,7 @@
  * please view the LICENSE file that was distributed with this source code.
  */
 
-namespace WellCommerce\Bundle\PageBundle\Generator;
+namespace WellCommerce\Bundle\PageBundle\Routing;
 
 use WellCommerce\Bundle\RoutingBundle\Generator\AbstractRouteGenerator;
 
