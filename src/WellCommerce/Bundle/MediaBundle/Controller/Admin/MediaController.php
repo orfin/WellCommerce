009958f98c880bb17d8de3141cc7f3788f48d485
Adam Piotrowski <adam@wellcommerce.org>
2016-01-15 00:01:24 +0100

    [STree] Added stree 'mediabundle' in src/WellCommerce/Bundle/MediaBundle

---
+++ src/WellCommerce/Bundle/MediaBundle/Controller/Admin/MediaController.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\MediaBundle\Controller\Admin;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use WellCommerce\Bundle\CoreBundle\Controller\Admin\AbstractAdminController;
+use WellCommerce\Bundle\MediaBundle\Exception\InvalidMediaException;
+
+/**
+ * Class MediaController
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class MediaController extends AbstractAdminController
+{
+    /**
+     * @var \WellCommerce\Bundle\MediaBundle\Manager\Admin\MediaManager
+     */
+    protected $manager;
+
+    /**
+     * Uploads and saves the file
+     *
+     * @param Request $request
+     *
+     * @return array|JsonResponse
+     */
+    public function addAction(Request $request)
+    {
+        $file   = $request->files->get('file');
+        $helper = $this->manager->getImageHelper();
+
+        try {
+            $media     = $this->manager->upload($file, 'images');
+            $thumbnail = $helper->getImage($media->getPath(), 'medium');
+
+            $response = [
+                'sId'        => $media->getId(),
+                'sThumb'     => $thumbnail,
+                'sFilename'  => $media->getName(),
+                'sExtension' => $media->getExtension(),
+                'sFileType'  => $media->getMime(),
+            ];
+        } catch (InvalidMediaException $e) {
+            $response = [
+                'sError'   => $this->trans('uploader.error'),
+                'sMessage' => $this->trans($e->getMessage()),
+            ];
+        }
+
+        return $this->jsonResponse($response);
+    }
+}
