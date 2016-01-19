50e15558382ffea22307b21e1ae0c7e650bca210
Adam Piotrowski <adam@wellcommerce.org>
2016-01-19 12:37:50 +0100

    [STree] Added stree 'localebundle' in src/WellCommerce/Bundle/LocaleBundle

---
+++ src/WellCommerce/Bundle/LocaleBundle/Controller/Front/LocaleController.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\LocaleBundle\Controller\Front;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use WellCommerce\Bundle\CoreBundle\Controller\Front\AbstractFrontController;
+
+/**
+ * Class LocaleController
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class LocaleController extends AbstractFrontController
+{
+    /**
+     * Redirects to home page
+     *
+     * @return RedirectResponse
+     */
+    public function switchAction(Request $request)
+    {
+        return new RedirectResponse($request->headers->get('referer'));
+    }
+}
