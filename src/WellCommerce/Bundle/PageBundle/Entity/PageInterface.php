5aa2d8f70b71a3b6de1ba2ddcda2b6f09a631354
Adam Piotrowski <adam@wellcommerce.org>
2016-01-19 12:38:00 +0100

    [STree] Added stree 'pagebundle' in src/WellCommerce/Bundle/PageBundle

---
+++ src/WellCommerce/Bundle/PageBundle/Entity/PageInterface.php
@@ -0,0 +1,114 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\PageBundle\Entity;
+
+use Doctrine\Common\Collections\Collection;
+use WellCommerce\Bundle\AppBundle\Entity\HierarchyAwareInterface;
+use WellCommerce\Bundle\CoreBundle\Entity\BlameableInterface;
+use WellCommerce\Bundle\CoreBundle\Entity\TimestampableInterface;
+use WellCommerce\Bundle\CoreBundle\Entity\TranslatableInterface;
+use WellCommerce\Bundle\ShopBundle\Entity\ShopCollectionAwareInterface;
+
+/**
+ * Interface PageInterface
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+interface PageInterface
+    extends TranslatableInterface, TimestampableInterface, BlameableInterface, HierarchyAwareInterface, ShopCollectionAwareInterface
+{
+    /**
+     * @return int
+     */
+    public function getId();
+
+    /**
+     * @return bool
+     */
+    public function getPublish();
+
+    /**
+     * @param bool $publish
+     */
+    public function setPublish($publish);
+
+    /**
+     * @return null|PageInterface
+     */
+    public function getParent();
+
+    /**
+     * @param PageInterface|null $parent
+     */
+    public function setParent(PageInterface $parent = null);
+
+    /**
+     * @return Collection
+     */
+    public function getChildren();
+
+    /**
+     * @param PageInterface $child
+     */
+    public function addChild(PageInterface $child);
+
+    /**
+     * @return string
+     */
+    public function getRedirectRoute();
+
+    /**
+     * @param string $redirectRoute
+     */
+    public function setRedirectRoute($redirectRoute);
+
+    /**
+     * @return string
+     */
+    public function getRedirectUrl();
+
+    /**
+     * @param string $redirectUrl
+     */
+    public function setRedirectUrl($redirectUrl);
+
+    /**
+     * @return Collection
+     */
+    public function getClientGroups();
+
+    /**
+     * @param Collection $clientGroups
+     */
+    public function setClientGroups(Collection $clientGroups);
+
+    /**
+     * @return int
+     */
+    public function getRedirectType();
+
+    /**
+     * @param int $redirectType
+     */
+    public function setRedirectType($redirectType);
+
+    /**
+     * @return string
+     */
+    public function getSection();
+
+    /**
+     * @param string $section
+     */
+    public function setSection($section);
+}
---
+++ src/WellCommerce/Bundle/PageBundle/Factory/PageFactory.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\PageBundle\Factory;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use WellCommerce\Bundle\CoreBundle\Factory\AbstractFactory;
+use WellCommerce\Bundle\PageBundle\Entity\PageInterface;
+
+/**
+ * Class PageFactory
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class PageFactory extends AbstractFactory
+{
+    /**
+     * @var string
+     */
+    protected $supportsInterface = PageInterface::class;
+
+    /**
+     * @return PageInterface
+     */
+    public function create()
+    {
+        /** @var  $page PageInterface */
+        $page = $this->init();
+        $page->setHierarchy(0);
+        $page->setCreatedAt(new \DateTime());
+        $page->setClientGroups(new ArrayCollection());
+        $page->setShops(new ArrayCollection());
+        $page->setParent(null);
+        $page->setPublish(true);
+        $page->setRedirectType(1);
+
+        return $page;
+    }
+}
