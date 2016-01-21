365d23b5ed77e9fd8c4af3856d53c349bfae644c
Adam Piotrowski <adam@wellcommerce.org>
2016-01-21 18:53:43 +0100

    [STree] Added stree 'taxbundle' in src/WellCommerce/Bundle/TaxBundle

---
+++ src/WellCommerce/Bundle/TaxBundle/Entity/TaxInterface.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\TaxBundle\Entity;
+
+use WellCommerce\Bundle\CoreBundle\Entity\BlameableInterface;
+use WellCommerce\Bundle\CoreBundle\Entity\TimestampableInterface;
+use WellCommerce\Bundle\CoreBundle\Entity\TranslatableInterface;
+
+/**
+ * Interface TaxInterface
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+interface TaxInterface extends TranslatableInterface, TimestampableInterface, BlameableInterface
+{
+    /**
+     * @return int
+     */
+    public function getId();
+
+    /**
+     * @return float
+     */
+    public function getValue();
+
+    /**
+     * @param float $value
+     */
+    public function setValue($value);
+}
