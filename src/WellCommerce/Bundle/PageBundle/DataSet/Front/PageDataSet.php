366651c3d3ce309aa71875bb4c698e11fe56b323
Adam Piotrowski <adam@wellcommerce.org>
2016-01-21 18:51:08 +0100

    [STree] Added stree 'pagebundle' in src/WellCommerce/Bundle/PageBundle

---
+++ src/WellCommerce/Bundle/PageBundle/DataSet/Front/PageDataSet.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\PageBundle\DataSet\Front;
+
+use WellCommerce\Bundle\PageBundle\DataSet\Admin\PageDataSet as BaseDataSet;
+use WellCommerce\Component\DataSet\Configurator\DataSetConfiguratorInterface;
+
+/**
+ * Class PageDataSet
+ *
+ * @author Adam Piotrowski <adam@wellcommerce.org>
+ */
+class PageDataSet extends BaseDataSet
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(DataSetConfiguratorInterface $configurator)
+    {
+        parent::configureOptions($configurator);
+
+        $configurator->setColumnTransformers([
+            'route' => $this->getDataSetTransformer('route')
+        ]);
+    }
+}
