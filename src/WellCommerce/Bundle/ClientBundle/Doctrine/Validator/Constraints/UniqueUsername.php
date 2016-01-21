6aa97225e71be3cccdfb0699c370fc53cc08b299
Adam Piotrowski <adam@wellcommerce.org>
2016-01-21 18:50:09 +0100

    [STree] Added stree 'clientbundle' in src/WellCommerce/Bundle/ClientBundle

---
+++ src/WellCommerce/Bundle/ClientBundle/Doctrine/Validator/Constraints/UniqueUsername.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\ClientBundle\Doctrine\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * Class UniqueUsername
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class UniqueUsername extends Constraint
+{
+    public $message    = 'You have an account already. Please login first.';
+    public $service    = 'client.orm.validator.unique';
+    public $fields     = [];
+    public $errorPath  = null;
+    public $ignoreNull = true;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validatedBy()
+    {
+        return $this->service;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTargets()
+    {
+        return self::CLASS_CONSTRAINT;
+    }
+}
