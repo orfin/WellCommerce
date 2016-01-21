3505229cf766ed2ac9ec484b13901a60b67181ee
Adam Piotrowski <adam@wellcommerce.org>
2016-01-21 18:49:25 +0100

    [STree] Added stree 'appbundle' in src/WellCommerce/Bundle/AppBundle

---
+++ src/WellCommerce/Bundle/AppBundle/DependencyInjection/WellCommerceAppExtension.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\AppBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use WellCommerce\Bundle\CoreBundle\DependencyInjection\AbstractExtension;
+
+/**
+ * Class WellCommerceAppExtension
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class WellCommerceAppExtension extends AbstractExtension
+{
+    CONST EXTENSION_NAME = 'well_commerce_app';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setExtensionConfiguration(ContainerBuilder $container, array $parameters = [])
+    {
+        $container->setParameter(self::EXTENSION_NAME, $parameters);
+    }
+}
