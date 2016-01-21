85c29a583cc4a8c5622f2c35ea4aa0d990ee904b
Adam Piotrowski <adam@wellcommerce.org>
2016-01-21 18:52:00 +0100

    [STree] Added stree 'productbundle' in src/WellCommerce/Bundle/ProductBundle

---
+++ src/WellCommerce/Bundle/ProductBundle/Controller/Front/ProductController.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\ProductBundle\Controller\Front;
+
+use WellCommerce\Bundle\CoreBundle\Controller\Front\AbstractFrontController;
+use WellCommerce\Bundle\CoreBundle\Service\Breadcrumb\BreadcrumbItem;
+use WellCommerce\Bundle\ProductBundle\Entity\ProductInterface;
+
+/**
+ * Class ProductController
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class ProductController extends AbstractFrontController
+{
+    public function indexAction(ProductInterface $product)
+    {
+        $this->addBreadCrumbItem(new BreadcrumbItem([
+            'name' => $product->translate()->getName(),
+        ]));
+
+        $this->manager->getProductContext()->setCurrentProduct($product);
+
+        return $this->displayTemplate('index', [
+            'product' => $product
+        ]);
+    }
+
+    public function viewAction(ProductInterface $product)
+    {
+        $this->manager->getProductContext()->setCurrentProduct($product);
+
+        $templateData       = $this->get('product.helper')->getProductDefaultTemplateData($product);
+        $basketModalContent = $this->renderView('WellCommerceAppBundle:Front/Product:view.html.twig', $templateData);
+
+        return $this->jsonResponse([
+            'basketModalContent' => $basketModalContent,
+            'attributes'         => $templateData['attributes']
+        ]);
+    }
+}
