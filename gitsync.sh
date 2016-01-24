65ba754972218bd9676285b5a86bc8a14124a51e
Adam Piotrowski <adam@wellcommerce.org>
2016-01-24 14:18:15 +0100

    Modified gitsync

--- gitsync.sh
+++ gitsync.sh
@@ -34,6 +34,7 @@ if [ "$action" == 'clear' ]
         git remote rm stree-oauthbundle
         git remote rm stree-pagebundle
         git remote rm stree-paymentbundle
+        git remote rm stree-pluginbundle
         git remote rm stree-producerbundle
         git remote rm stree-productbundle
         git remote rm stree-productstatusbundle
@@ -79,6 +80,7 @@ if [ "$action" == 'clear' ]
         git branch -D stree-backports-oauthbundle
         git branch -D stree-backports-pagebundle
         git branch -D stree-backports-paymentbundle
+        git branch -D stree-backports-pluginbundle
         git branch -D stree-backports-producerbundle
         git branch -D stree-backports-productbundle
         git branch -D stree-backports-productstatusbundle
@@ -131,6 +133,7 @@ if [ "$action" == 'init' ]
         git stree add OAuthBundle -P src/WellCommerce/Bundle/OAuthBundle git@github.com:WellCommerce/OAuthBundle.git
         git stree add OrderBundle -P src/WellCommerce/Bundle/OrderBundle git@github.com:WellCommerce/OrderBundle.git
         git stree add PaymentBundle -P src/WellCommerce/Bundle/PaymentBundle git@github.com:WellCommerce/PaymentBundle.git
+        git stree add PluginBundle -P src/WellCommerce/Bundle/PluginBundle git@github.com:WellCommerce/PluginBundle.git
         git stree add ProductBundle -P src/WellCommerce/Bundle/ProductBundle git@github.com:WellCommerce/ProductBundle.git
         git stree add ProductStatusBundle -P src/WellCommerce/Bundle/ProductStatusBundle git@github.com:WellCommerce/ProductStatusBundle.git
         git stree add ProducerBundle -P src/WellCommerce/Bundle/ProducerBundle git@github.com:WellCommerce/ProducerBundle.git
@@ -179,6 +182,7 @@ if [ "$action" == 'push' ]
         git stree push OAuthBundle
         git stree push PageBundle
         git stree push PaymentBundle
+        git stree push PluginBundle
         git stree push ProducerBundle
         git stree push ProductBundle
         git stree push ProductStatusBundle
