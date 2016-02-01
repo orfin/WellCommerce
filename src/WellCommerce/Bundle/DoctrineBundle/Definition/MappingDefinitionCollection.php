7e8904a7010991332273c99b88fb5fbc80f6d8fe
Adam Piotrowski <adam@wellcommerce.org>
2016-02-01 10:21:08 +0100

    DoctrineBundle

---
+++ src/WellCommerce/Bundle/DoctrineBundle/Definition/MappingDefinitionCollection.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\DoctrineBundle\Definition;
+
+use WellCommerce\Component\Collections\ArrayCollection;
+
+/**
+ * Class MappingDefinitionCollection
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class MappingDefinitionCollection extends ArrayCollection
+{
+    /**
+     * @param MappingDefinitionInterface $mappingDefinition
+     */
+    public function add(MappingDefinitionInterface $mappingDefinition)
+    {
+        $this->items[] = $mappingDefinition;
+    }
+
+    /**
+     * @return MappingDefinitionInterface[]
+     */
+    public function all()
+    {
+        return $this->items;
+    }
+}
