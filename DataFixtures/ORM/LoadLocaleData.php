505ab5afd1ef8aa545e387c8ebded6d39397abc5
Adam Piotrowski <adam@wellcommerce.org>
2016-02-03 22:26:22 +0100

    Moved AbstractDataFixture to DoctrineBundle
    (cherry picked from commit 12649e3058ccffb8260b25b2654d829c418251a8)

--- DataFixtures/ORM/LoadLocaleData.php
+++ DataFixtures/ORM/LoadLocaleData.php
@@ -13,7 +13,7 @@
 namespace WellCommerce\Bundle\LocaleBundle\DataFixtures\ORM;
 
 use Doctrine\Common\Persistence\ObjectManager;
-use WellCommerce\Bundle\CoreBundle\DataFixtures\AbstractDataFixture;
+use WellCommerce\Bundle\DoctrineBundle\DataFixtures\AbstractDataFixture;
 use WellCommerce\Bundle\LocaleBundle\Entity\Locale;
 
 /**
