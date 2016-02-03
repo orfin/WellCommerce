61622ba9b23068095e9f0ec8be814b29b8e9d45e
Adam Piotrowski <adam@wellcommerce.org>
2016-02-03 22:26:22 +0100

    Moved AbstractDataFixture to DoctrineBundle
    (cherry picked from commit 12649e3058ccffb8260b25b2654d829c418251a8)

--- DataFixtures/ORM/LoadProductStatusData.php
+++ DataFixtures/ORM/LoadProductStatusData.php
@@ -13,7 +13,7 @@
 namespace WellCommerce\Bundle\ProductStatusBundle\DataFixtures\ORM;
 
 use Doctrine\Common\Persistence\ObjectManager;
-use WellCommerce\Bundle\CoreBundle\DataFixtures\AbstractDataFixture;
+use WellCommerce\Bundle\DoctrineBundle\DataFixtures\AbstractDataFixture;
 use WellCommerce\Bundle\CoreBundle\Helper\Sluggable;
 use WellCommerce\Bundle\ProductStatusBundle\Entity\ProductStatus;
 
