9beb15b3927b47b73ae6263966f0eb2fd097734e
Adam Piotrowski <adam@wellcommerce.org>
2016-02-03 22:47:17 +0100

    Moved Repository from CoreBundle to DoctrineBundle
    (cherry picked from commit af34d7a6b28353f0d8fb0b5f21d804295e26d48e)

--- Repository/AvailabilityRepository.php
+++ Repository/AvailabilityRepository.php
@@ -11,7 +11,7 @@
  */
 namespace WellCommerce\Bundle\AvailabilityBundle\Repository;
 
-use WellCommerce\Bundle\CoreBundle\Repository\AbstractEntityRepository;
+use WellCommerce\Bundle\DoctrineBundle\Repository\AbstractEntityRepository;
 
 /**
  * Class AvailabilityRepository
