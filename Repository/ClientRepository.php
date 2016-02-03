c3c2697a88237b5f2a527a9f6bdfced0a6bd5efa
Adam Piotrowski <adam@wellcommerce.org>
2016-02-03 22:47:17 +0100

    Moved Repository from CoreBundle to DoctrineBundle
    (cherry picked from commit af34d7a6b28353f0d8fb0b5f21d804295e26d48e)

--- Repository/ClientRepository.php
+++ Repository/ClientRepository.php
@@ -17,7 +17,7 @@ use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use WellCommerce\Bundle\AdminBundle\Repository\UserRepositoryInterface;
-use WellCommerce\Bundle\CoreBundle\Repository\AbstractEntityRepository;
+use WellCommerce\Bundle\DoctrineBundle\Repository\AbstractEntityRepository;
 
 /**
  * Class ClientGroupRepository
