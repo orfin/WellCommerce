a2e04903601e63e43521a7ce8c4f0200f12aabf8
Adam Piotrowski <adam@wellcommerce.org>
2016-02-03 22:26:22 +0100

    Moved AbstractDataFixture to DoctrineBundle
    (cherry picked from commit 12649e3058ccffb8260b25b2654d829c418251a8)

--- DataFixtures/ORM/LoadCouponData.php
+++ DataFixtures/ORM/LoadCouponData.php
@@ -13,7 +13,7 @@
 namespace WellCommerce\Bundle\CouponBundle\DataFixtures\ORM;
 
 use Doctrine\Common\Persistence\ObjectManager;
-use WellCommerce\Bundle\CoreBundle\DataFixtures\AbstractDataFixture;
+use WellCommerce\Bundle\DoctrineBundle\DataFixtures\AbstractDataFixture;
 
 /**
  * Class LoadCouponData
