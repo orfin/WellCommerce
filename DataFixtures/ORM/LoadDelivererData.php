ee5051b02c571c59f25e03b62a8f21b74934741d
Adam Piotrowski <adam@wellcommerce.org>
2016-02-03 22:26:22 +0100

    Moved AbstractDataFixture to DoctrineBundle
    (cherry picked from commit 12649e3058ccffb8260b25b2654d829c418251a8)

--- DataFixtures/ORM/LoadDelivererData.php
+++ DataFixtures/ORM/LoadDelivererData.php
@@ -13,7 +13,7 @@
 namespace WellCommerce\Bundle\DelivererBundle\DataFixtures\ORM;
 
 use Doctrine\Common\Persistence\ObjectManager;
-use WellCommerce\Bundle\CoreBundle\DataFixtures\AbstractDataFixture;
+use WellCommerce\Bundle\DoctrineBundle\DataFixtures\AbstractDataFixture;
 use WellCommerce\Bundle\DelivererBundle\Entity\Deliverer;
 
 /**
