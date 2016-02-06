3c870557a465ddf976392a9f327bb29216038d4d
Adam Piotrowski <adam@wellcommerce.org>
2016-02-06 10:47:24 +0100

    DistributionBundle
    (cherry picked from commit 73586a19c308bb626cbc98c3e5c781a5680c7039)

--- EventListener/OrderSubscriber.php
+++ EventListener/OrderSubscriber.php
@@ -11,7 +11,7 @@
  */
 namespace WellCommerce\Bundle\OrderBundle\EventListener;
 
-use WellCommerce\Bundle\CoreBundle\Event\ResourceEvent;
+use WellCommerce\Bundle\DoctrineBundle\Event\ResourceEvent;
 use WellCommerce\Bundle\CoreBundle\EventListener\AbstractEventSubscriber;
 use WellCommerce\Bundle\OrderBundle\Entity\OrderInterface;
 use WellCommerce\Bundle\OrderBundle\Entity\OrderTotalDetailInterface;
