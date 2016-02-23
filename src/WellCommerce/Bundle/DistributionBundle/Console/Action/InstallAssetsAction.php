cd4daa8aa5295cd49b70a4844d794a263571775f
Adam Piotrowski <adam@wellcommerce.org>
2016-02-23 12:02:31 +0100

    [STree] Added stree 'distributionbundle' in src/WellCommerce/Bundle/DistributionBundle

---
+++ src/WellCommerce/Bundle/DistributionBundle/Console/Action/InstallAssetsAction.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\DistributionBundle\Console\Action;
+
+/**
+ * Class InstallAssetsAction
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class InstallAssetsAction implements ConsoleActionInterface
+{
+    public function getCommandsToExecute()
+    {
+        return [
+            'assets:install'              => [],
+            'bazinga:js-translation:dump' => [],
+            'fos:js-routing:dump'         => [],
+            'assetic:dump'                => []
+        ];
+    }
+}
