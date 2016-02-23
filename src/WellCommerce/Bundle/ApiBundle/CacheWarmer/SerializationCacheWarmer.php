97113f8a4b122cb6f590f6d7787d0d1df1bfdc83
Adam Piotrowski <adam@wellcommerce.org>
2016-02-23 12:01:39 +0100

    [STree] Added stree 'apibundle' in src/WellCommerce/Bundle/ApiBundle

---
+++ src/WellCommerce/Bundle/ApiBundle/CacheWarmer/SerializationCacheWarmer.php
@@ -0,0 +1,115 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\ApiBundle\CacheWarmer;
+
+use Doctrine\Common\Persistence\Mapping\ClassMetadata;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpKernel\CacheWarmer\CacheWarmer;
+use Symfony\Component\Yaml\Yaml;
+use WellCommerce\Bundle\ApiBundle\Metadata\Loader\SerializationMetadataLoaderInterface;
+use WellCommerce\Bundle\DoctrineBundle\Helper\Doctrine\DoctrineHelperInterface;
+
+/**
+ * Class SerializationCacheWarmer
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class SerializationCacheWarmer extends CacheWarmer
+{
+    /**
+     * @var DoctrineHelperInterface
+     */
+    protected $doctrineHelper;
+
+    /**
+     * @var Filesystem
+     */
+    protected $filesystem;
+
+    /**
+     * SerializationCacheWarmer constructor.
+     *
+     * @param DoctrineHelperInterface $doctrineHelper
+     */
+    public function __construct(DoctrineHelperInterface $doctrineHelper)
+    {
+        $this->doctrineHelper = $doctrineHelper;
+        $this->filesystem     = new Filesystem();
+    }
+
+    /**
+     * Warms up the cache.
+     *
+     * @param string $cacheDir The cache directory
+     */
+    public function warmUp($cacheDir)
+    {
+        $configuration = $this->getConfiguration();
+
+        if (count($configuration)) {
+            $file = $cacheDir . '/' . SerializationMetadataLoaderInterface::CACHE_FILENAME;
+            $this->writeCacheFile($file, sprintf('<?php return %s;', var_export($configuration, true)));
+        }
+    }
+
+    /**
+     * @return array
+     */
+    protected function getConfiguration()
+    {
+        $configuration      = [];
+        $metadataCollection = $this->doctrineHelper->getMetadataFactory()->getAllMetadata();
+
+        foreach ($metadataCollection as $entityMetadata) {
+            $this->appendConfigurationFromMetadata($entityMetadata, $configuration);
+        }
+
+        return $configuration;
+    }
+
+    protected function appendConfigurationFromMetadata(ClassMetadata $metadata, &$configuration)
+    {
+        $serializationFilePath = $this->resolvePath($metadata->getReflectionClass());
+        if ($this->filesystem->exists($serializationFilePath)) {
+            $content       = file_get_contents($serializationFilePath);
+            $configuration = array_replace_recursive($configuration, $this->parseContent($content));
+        }
+    }
+
+    protected function resolvePath(\ReflectionClass $reflectionClass)
+    {
+        return dirname($reflectionClass->getFileName()) . '/../Resources/config/serialization/' . $reflectionClass->getShortName() . '.yml';
+    }
+
+    /**
+     * Parses the Yaml contents
+     *
+     * @param string $content
+     *
+     * @return array
+     */
+    private function parseContent($content)
+    {
+        return Yaml::parse($content);
+    }
+
+    /**
+     * Checks whether this warmer is optional or not.
+     *
+     * @return Boolean always true
+     */
+    public function isOptional()
+    {
+        return false;
+    }
+}
---
+++ src/WellCommerce/Bundle/ApiBundle/Command/GenerateSerializationMetadataCommand.php
@@ -0,0 +1,139 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\ApiBundle\Command;
+
+use Doctrine\Common\Inflector\Inflector;
+use Doctrine\Common\Persistence\Mapping\ClassMetadata;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Yaml\Yaml;
+use WellCommerce\Bundle\DoctrineBundle\Helper\Doctrine\DoctrineHelperInterface;
+
+/**
+ * Class GenerateMetadataCommand
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class GenerateSerializationMetadataCommand extends Command
+{
+    protected $hiddenFields = ['createdBy', 'updatedBy', 'deletedBy', 'password', 'salt'];
+
+    /**
+     * @var DoctrineHelperInterface
+     */
+    protected $doctrineHelper;
+
+    /**
+     * @var Filesystem
+     */
+    protected $filesystem;
+
+    /**
+     * GenerateMetadataCommand constructor.
+     *
+     * @param DoctrineHelperInterface $doctrineHelper
+     */
+    public function __construct(DoctrineHelperInterface $doctrineHelper)
+    {
+        parent::__construct();
+        $this->doctrineHelper = $doctrineHelper;
+        $this->filesystem     = new Filesystem();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setDescription('Generates serialization metadata for entities');
+        $this->setName('api:generate:metadata');
+        $this->addOption('force', null, InputOption::VALUE_NONE, 'Force to overwrite existing configuration files.');
+    }
+
+    /**
+     * Executes the actions
+     *
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $metadataCollection = $this->doctrineHelper->getMetadataFactory()->getAllMetadata();
+        $force              = true === $input->getOption('force');
+
+        foreach ($metadataCollection as $entityMetadata) {
+            $this->dumpSerializationFile($entityMetadata, $output, $force);
+        }
+    }
+
+    protected function dumpSerializationFile(ClassMetadata $metadata, OutputInterface $output, $force = false)
+    {
+        $targetPath = $this->resolvePath($metadata->getReflectionClass());
+
+        $config = [
+            $metadata->getName() => [
+                'fields'       => $this->prepareMetadataFields($metadata),
+                'associations' => $this->prepareMetadataAssociations($metadata),
+                'callbacks'    => [],
+            ]
+        ];
+
+        $message = 'Dumped serialization config for <info>%s</info>';
+
+        if ($force) {
+            $this->filesystem->dumpFile($targetPath, Yaml::dump($config, 6));
+        } else {
+            if (false === $this->filesystem->exists($targetPath)) {
+                $this->filesystem->dumpFile($targetPath, Yaml::dump($config, 6));
+            } else {
+                $message = 'Skipped dumping serialization config for <info>%s</info>';
+            }
+        }
+
+        $output->write(sprintf($message, $metadata->getName()), true);
+    }
+
+    protected function prepareMetadataFields(ClassMetadata $metadata)
+    {
+        $fields = [];
+
+        foreach ($metadata->getFieldNames() as $fieldName) {
+            $groups = [];
+            list($realName,) = explode('.', $fieldName);
+            if (!in_array($realName, $this->hiddenFields)) {
+                $groups = ['default', Inflector::tableize($metadata->getReflectionClass()->getShortName())];
+            }
+            $fields[$realName]['groups'] = $groups;
+        }
+
+        return $fields;
+    }
+
+    protected function prepareMetadataAssociations(ClassMetadata $metadata)
+    {
+        $associations = [];
+        foreach ($metadata->getAssociationNames() as $associationName) {
+            $associations[$associationName]['groups'] = [Inflector::tableize($metadata->getReflectionClass()->getShortName())];
+        }
+
+        return $associations;
+    }
+
+    protected function resolvePath(\ReflectionClass $reflectionClass)
+    {
+        return dirname($reflectionClass->getFileName()) . '/../Resources/config/serialization/' . $reflectionClass->getShortName() . '.yml';
+    }
+}
