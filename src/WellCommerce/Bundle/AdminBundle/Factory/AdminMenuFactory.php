56c6739f4c6fd6d2f2e23c1883f202421047b471
Adam Piotrowski <adam@wellcommerce.org>
2016-02-23 12:01:24 +0100

    [STree] Added stree 'adminbundle' in src/WellCommerce/Bundle/AdminBundle

---
+++ src/WellCommerce/Bundle/AdminBundle/Factory/AdminMenuFactory.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\AdminBundle\Factory;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use WellCommerce\Bundle\AdminBundle\Entity\AdminMenuInterface;
+use WellCommerce\Bundle\CoreBundle\Factory\AbstractFactory;
+
+/**
+ * Class AdminMenuFactory
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class AdminMenuFactory extends AbstractFactory
+{
+    /**
+     * @var string
+     */
+    protected $supportsInterface = AdminMenuInterface::class;
+
+    /**
+     * @return AdminMenuInterface
+     */
+    public function create()
+    {
+        $adminMenu = $this->init();
+        $adminMenu->setParent(null);
+        $adminMenu->setCssClass('');
+        $adminMenu->setChildren(new ArrayCollection());
+
+        return $adminMenu;
+    }
+}
