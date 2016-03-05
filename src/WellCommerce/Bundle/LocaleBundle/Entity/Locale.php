6ff2bb79e645bd0559b48381c1fb1561e3c8ebd0
Adam Piotrowski <adam@wellcommerce.org>
2016-03-05 10:13:13 +0100

    [STree] Added stree 'localebundle' in src/WellCommerce/Bundle/LocaleBundle

---
+++ src/WellCommerce/Bundle/LocaleBundle/Entity/Locale.php
@@ -0,0 +1,63 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\LocaleBundle\Entity;
+
+use Knp\DoctrineBehaviors\Model\Blameable\Blameable;
+use Knp\DoctrineBehaviors\Model\Timestampable\Timestampable;
+use WellCommerce\Bundle\CurrencyBundle\Entity\CurrencyAwareTrait;
+
+/**
+ * Class Locale
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class Locale implements LocaleInterface
+{
+    use Timestampable;
+    use Blameable;
+    use CurrencyAwareTrait;
+
+    /**
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $code;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+    }
+}
