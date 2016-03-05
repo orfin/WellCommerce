52f4214bda19a30edae20ecefcefbc858418e624
Adam Piotrowski <adam@wellcommerce.org>
2016-03-05 10:14:02 +0100

    [STree] Added stree 'oauthbundle' in src/WellCommerce/Bundle/OAuthBundle

---
+++ src/WellCommerce/Bundle/OAuthBundle/Controller/FacebookController.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\OAuthBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use WellCommerce\Bundle\CoreBundle\Controller\AbstractController;
+
+/**
+ * Class FacebookController
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class FacebookController extends AbstractController
+{
+    public function connectAction(Request $request)
+    {
+        return $this->get('oauth.authentificator.facebook')->start($request);
+    }
+
+    public function checkAction(Request $request)
+    {
+
+    }
+}
