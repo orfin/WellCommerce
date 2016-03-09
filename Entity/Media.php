4897c5e45c6619c13432753f96c9526ab0ba46a0
Adam Piotrowski <adam@wellcommerce.org>
2016-03-09 01:57:31 +0100

    Implemented MediaAware
    (cherry picked from commit 002b878058585aa5ec74970eef963635df39f4f8)

--- Entity/Media.php
+++ Entity/Media.php
@@ -158,5 +158,4 @@ class Media implements MediaInterface
             $this->path = sprintf('%s.%s', $filename, $this->getExtension());
         }
     }
-
 }
---
+++ Entity/MediaAwareTrait.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\MediaBundle\Entity;
+
+/**
+ * Class MediaAwareTrait
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+trait MediaAwareTrait
+{
+    /**
+     * @var MediaInterface
+     */
+    protected $photo;
+
+    /**
+     * @return MediaInterface
+     */
+    public function getPhoto()
+    {
+        return $this->photo;
+    }
+
+    /**
+     * @param null|MediaInterface $photo
+     */
+    public function setPhoto(MediaInterface $photo = null)
+    {
+        $this->photo = $photo;
+    }
+}
