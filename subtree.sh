eea713403619a0d218d7e4483802e09ae44583d6
Adam Piotrowski <adam@wellcommerce.org>
2016-03-09 21:09:31 +0100

    Resync

--- subtree.sh
+++ subtree.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-
-for path in `git config -f .gitsubtrees -l | grep 'path=' | cut -d '=' -f2 | uniq`; do
-    url=`git config -f .gitsubtrees subtree.$path.url`
-    branch=`git config -f .gitsubtrees subtree.$path.branch`
-    remote=`git config -f .gitsubtrees subtree.$path.remote`
-
-    if [ -d "$path" ] ; then
-        echo "$path is existed"
-    else
-        git subtree add --prefix $path $url $branch --squash
-    fi
-done
+git subtree add --prefix=UnitBundle git@github.com:WellCommerce/UnitBundle.git master
