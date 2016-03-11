e83070c4828a7a4bbd0f92fd8f8fa7827a346487
Adam Piotrowski <adam@wellcommerce.org>
2016-03-11 17:54:40 +0100

    Controller return types

--- Entity/AttributeTranslation.php
+++ Entity/AttributeTranslation.php
@@ -48,7 +48,7 @@ class AttributeTranslation implements LocaleAwareInterface
     /**
      * {@inheritdoc}
      */
-    public function getCopyingSensitiveProperties()
+    public function getCopyingSensitiveProperties() : array
     {
         return [
             'name'
