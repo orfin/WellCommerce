09f525c61b45f87c20e7ec5c3440dff657b916f8
Adam Piotrowski <adam@wellcommerce.org>
2016-03-12 14:43:54 +0100

    Refactoring of locale add command, added Copier

--- src/WellCommerce/Bundle/LocaleBundle/Command/AddLocaleCommand.php
+++ src/WellCommerce/Bundle/LocaleBundle/Command/AddLocaleCommand.php
@@ -12,8 +12,6 @@
 
 namespace WellCommerce\Bundle\LocaleBundle\Command;
 
-use Doctrine\Common\Collections\Criteria;
-use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Symfony\Component\Console\Exception\LogicException;
@@ -21,14 +19,9 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ChoiceQuestion;
 use Symfony\Component\Intl\Intl;
-use Symfony\Component\PropertyAccess\PropertyAccess;
-use WellCommerce\Bundle\CurrencyBundle\Entity\CurrencyInterface;
-use WellCommerce\Bundle\CurrencyBundle\Repository\CurrencyRepositoryInterface;
-use WellCommerce\Bundle\DoctrineBundle\Helper\Doctrine\DoctrineHelperInterface;
-use WellCommerce\Bundle\LocaleBundle\Entity\LocaleAwareInterface;
-use WellCommerce\Bundle\LocaleBundle\Entity\LocaleInterface;
-use WellCommerce\Bundle\LocaleBundle\Factory\LocaleFactory;
-use WellCommerce\Bundle\LocaleBundle\Repository\LocaleRepositoryInterface;
+use WellCommerce\Bundle\CurrencyBundle\DataSet\Admin\CurrencyDataSet;
+use WellCommerce\Bundle\LocaleBundle\DataSet\Admin\LocaleDataSet;
+use WellCommerce\Bundle\LocaleBundle\Manager\Admin\LocaleManager;
 
 /**
  * Class AddLocaleCommand
@@ -38,50 +31,48 @@ use WellCommerce\Bundle\LocaleBundle\Repository\LocaleRepositoryInterface;
 class AddLocaleCommand extends Command
 {
     /**
-     * @var DoctrineHelperInterface
+     * @var LocaleManager
      */
-    protected $doctrineHelper;
+    protected $localeManager;
 
     /**
-     * @var LocaleRepositoryInterface
+     * @var LocaleDataSet
      */
-    protected $localeRepository;
+    protected $localeDataSet;
 
     /**
-     * @var LocaleFactory
+     * @var CurrencyDataSet
      */
-    protected $localeFactory;
+    protected $currencyDataSet;
 
     /**
-     * @var CurrencyRepositoryInterface
+     * @var array
      */
-    protected $currencyRepository;
+    protected $installedLocales = [];
 
     /**
-     * @var \Symfony\Component\PropertyAccess\PropertyAccessor
+     * @var array
      */
-    protected $propertyAccessor;
+    protected $installedCurrencies = [];
+
+    /**
+     * @var array
+     */
+    protected $availableLocales = [];
 
     /**
      * AddLocaleCommand constructor.
      *
-     * @param DoctrineHelperInterface     $doctrineHelper
-     * @param LocaleRepositoryInterface   $localeRepository
-     * @param LocaleFactory               $localeFactory
-     * @param CurrencyRepositoryInterface $currencyRepository
+     * @param LocaleManager   $localeManager
+     * @param LocaleDataSet   $localeDataSet
+     * @param CurrencyDataSet $currencyDataSet
      */
-    public function __construct(
-        DoctrineHelperInterface $doctrineHelper,
-        LocaleRepositoryInterface $localeRepository,
-        LocaleFactory $localeFactory,
-        CurrencyRepositoryInterface $currencyRepository
-    ) {
+    public function __construct(LocaleManager $localeManager, LocaleDataSet $localeDataSet, CurrencyDataSet $currencyDataSet)
+    {
         parent::__construct();
-        $this->doctrineHelper     = $doctrineHelper;
-        $this->localeRepository   = $localeRepository;
-        $this->localeFactory      = $localeFactory;
-        $this->currencyRepository = $currencyRepository;
-        $this->propertyAccessor   = PropertyAccess::createPropertyAccessor();
+        $this->localeManager   = $localeManager;
+        $this->localeDataSet   = $localeDataSet;
+        $this->currencyDataSet = $currencyDataSet;
     }
 
     /**
@@ -93,6 +84,13 @@ class AddLocaleCommand extends Command
         $this->setName('wellcommerce:locale:add');
     }
 
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $this->installedLocales    = $this->getInstalledLocales();
+        $this->installedCurrencies = $this->getInstalledCurrencies();
+        $this->availableLocales    = $this->getAvailableLocales();
+    }
+
     /**
      * Executes the actions
      *
@@ -101,173 +99,74 @@ class AddLocaleCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $sourceLocales       = $this->getSourceLocales();
-        $defaultSourceLocale = current($sourceLocales);
-        $targetLocales       = $this->getTargetLocales($sourceLocales);
-        $currencies          = $this->getInstalledCurrencies();
-        $helper              = $this->getHelper('question');
-        $question            = new ChoiceQuestion(
-            sprintf(
-                'Please select source locale from which new entities will be copied (defaults to "%s"):',
-                $defaultSourceLocale
-            ),
-            $sourceLocales,
-            $defaultSourceLocale
-        );
+        $sourceLocale   = $this->chooseSourceLocale($input, $output);
+        $targetLocale   = $this->chooseTargetLocale($input, $output);
+        $targetCurrency = $this->chooseTargetCurrency($input, $output);
 
-        $sourceLocale = $helper->ask($input, $output, $question);
-        $question     = new ChoiceQuestion('Please select target locale to which new entities will be copied:', $targetLocales, null);
-        $targetLocale = $helper->ask($input, $output, $question);
-
-        if ($sourceLocale === $targetLocale) {
-            throw new LogicException('Source locale cannot be the same as target locale');
-        }
-
-        $question             = new ChoiceQuestion('Please select a default currency for new locale:', $currencies, null);
-        $targetLocaleCurrency = $helper->ask($input, $output, $question);
-        $locale               = $this->createLocale($targetLocale, $targetLocaleCurrency);
+        $locale = $this->localeManager->createLocale($targetLocale, $targetCurrency);
         $output->writeln(sprintf('<info>Created a new locale "%s"</info>', $locale->getCode()));
 
-        $this->copyLocaleData($sourceLocale, $locale, $output);
+        $this->localeManager->copyLocaleData($sourceLocale, $targetLocale);
 
-        $this->doctrineHelper->getEntityManager()->flush();
+        $output->writeln(sprintf('<info>Finished copying "%s" data</info>', $locale->getCode()));
     }
 
-    /**
-     * Copies the locale data from source locale to target locale
-     *
-     * @param string          $sourceLocale
-     * @param LocaleInterface $targetLocale
-     * @param OutputInterface $output
-     */
-    protected function copyLocaleData($sourceLocale, LocaleInterface $targetLocale, OutputInterface $output)
+    protected function chooseSourceLocale(InputInterface $input, OutputInterface $output) : string
     {
-        $entityManager = $this->doctrineHelper->getEntityManager();
-        $metadata      = $this->doctrineHelper->getAllMetadata();
-        foreach ($metadata as $classMetadata) {
-            $reflectionClass = $classMetadata->getReflectionClass();
-            if ($reflectionClass->implementsInterface(\WellCommerce\Bundle\LocaleBundle\Entity\LocaleAwareInterface::class)) {
-                $repository = $entityManager->getRepository($reflectionClass->getName());
-                $this->copyTranslatableEntities($repository, $sourceLocale, $targetLocale->getCode(), $output);
-            }
-        }
+        $defaultLocale = current($this->installedLocales);
+        $question      = new ChoiceQuestion(
+            sprintf(
+                'Please select source locale from which new entities will be copied (defaults to "%s"):',
+                $defaultLocale
+            ),
+            $this->installedLocales,
+            $defaultLocale
+        );
+
+        $sourceLocale = $this->getHelper('question')->ask($input, $output, $question);
+
+        return $sourceLocale;
     }
 
-    /**
-     * Copies translatable entities between locales using a repository collection
-     *
-     * @param EntityRepository $repository
-     * @param string           $sourceLocale
-     * @param string  $targetLocale
-     * @param OutputInterface  $output
-     */
-    protected function copyTranslatableEntities(
-        EntityRepository $repository,
-        $sourceLocale,
-        $targetLocale,
-        OutputInterface $output
-    ) {
-        $entityManager = $this->doctrineHelper->getEntityManager();
-        $criteria      = new Criteria();
-        $criteria->where($criteria->expr()->eq('locale', $sourceLocale));
-        $collection = $repository->matching($criteria);
-
-        $collection->map(function (LocaleAwareInterface $entity) use ($targetLocale, $entityManager) {
-            $duplicate = clone $entity;
-            foreach ($entity->getCopyingSensitiveProperties() as $propertyName) {
-                $value = sprintf('%s-%s', $this->propertyAccessor->getValue($entity, $propertyName), $targetLocale);
-                $this->propertyAccessor->setValue($duplicate, $propertyName, $value);
-                $duplicate->setLocale($targetLocale);
-                $entityManager->persist($duplicate);
-            }
-        });
-
-        $output->write(sprintf(
-            'Copied <info>%s</info> entities <info>%s</info>',
-            $collection->count(),
-            $repository->getClassName()
-        ), true);
-
-    }
-
-    /**
-     * Creates a new locale
-     *
-     * @param string $localeCode
-     *
-     * @return LocaleInterface
-     */
-    protected function createLocale($localeCode, $targetLocaleCurrency)
+    protected function chooseTargetLocale(InputInterface $input, OutputInterface $output) : string
     {
-        $currency = $this->currencyRepository->findOneBy(['code' => $targetLocaleCurrency]);
-        if (!$currency instanceof CurrencyInterface) {
-            throw new InvalidArgumentException(sprintf('Wrong default currency "%s" was given', $targetLocaleCurrency));
-        }
+        $question     = new ChoiceQuestion('Please select target locale to which new entities will be copied:', $this->availableLocales);
+        $targetLocale = $this->getHelper('question')->ask($input, $output, $question);
 
-        $locale = $this->localeFactory->create();
-        $locale->setCode($localeCode);
-        $locale->setEnabled(true);
-        $locale->setCurrency($currency);
-        $this->doctrineHelper->getEntityManager()->persist($locale);
-
-        return $locale;
+        return $targetLocale;
     }
 
-    /**
-     * @return array
-     */
-    protected function getInstalledCurrencies()
+    protected function chooseTargetCurrency(InputInterface $input, OutputInterface $output) : string
     {
-        $currencies = [];
-        $collection = $this->currencyRepository->matching(new Criteria());
+        $question             = new ChoiceQuestion('Please select a default currency for new locale:', $this->installedCurrencies);
+        $targetLocaleCurrency = $this->getHelper('question')->ask($input, $output, $question);
 
-        $collection->map(function (CurrencyInterface $currency) use (&$currencies) {
-            $currencies[$currency->getCode()] = $currency->getCode();
-        });
-
-        return $currencies;
+        return $targetLocaleCurrency;
     }
 
-    /**
-     * @return array
-     */
-    protected function getSourceLocales()
+    protected function getInstalledCurrencies() : array
     {
-        $locales    = [];
-        $collection = $this->localeRepository->matching(new Criteria());
-
-        $collection->map(function (LocaleInterface $locale) use (&$locales) {
-            $locales[$locale->getCode()] = $locale->getCode();
-        });
-
-        return $locales;
+        return $this->currencyDataSet->getResult('select', ['order_by' => 'code'], [
+            'label_column' => 'code',
+            'value_column' => 'code'
+        ]);
     }
 
-    /**
-     * @return array
-     */
-    protected function getTargetCurrencies()
+    protected function getInstalledLocales() : array
     {
-        $locales    = [];
-        $collection = $this->localeRepository->matching(new Criteria());
-
-        $collection->map(function (LocaleInterface $locale) use (&$locales) {
-            $locales[$locale->getCode()] = $locale->getCode();
-        });
-
-        return $locales;
+        return $this->localeDataSet->getResult('select', ['order_by' => 'code'], [
+            'label_column' => 'code',
+            'value_column' => 'code'
+        ]);
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getTargetLocales($sourceLocales)
+    public function getAvailableLocales() : array
     {
         $locales    = [];
         $collection = Intl::getLocaleBundle()->getLocaleNames();
 
         foreach ($collection as $locale => $name) {
-            if (!in_array($locale, $sourceLocales)) {
+            if (!in_array($locale, $this->installedLocales)) {
                 $locales[$locale] = $name;
             }
         }
--- src/WellCommerce/Bundle/LocaleBundle/Manager/Admin/LocaleManager.php
+++ src/WellCommerce/Bundle/LocaleBundle/Manager/Admin/LocaleManager.php
@@ -13,6 +13,9 @@
 namespace WellCommerce\Bundle\LocaleBundle\Manager\Admin;
 
 use WellCommerce\Bundle\CoreBundle\Manager\Admin\AbstractAdminManager;
+use WellCommerce\Bundle\CurrencyBundle\Entity\CurrencyInterface;
+use WellCommerce\Bundle\LocaleBundle\Copier\LocaleCopierInterface;
+use WellCommerce\Bundle\LocaleBundle\Entity\LocaleInterface;
 
 /**
  * Class LocaleManager
@@ -21,4 +24,38 @@ use WellCommerce\Bundle\CoreBundle\Manager\Admin\AbstractAdminManager;
  */
 class LocaleManager extends AbstractAdminManager
 {
+    public function createLocale(string $localeCode, string $targetLocaleCurrency) : LocaleInterface
+    {
+        $currency = $this->getTargetCurrency($targetLocaleCurrency);
+        $locale   = $this->initResource();
+        $locale->setCode($localeCode);
+        $locale->setEnabled(true);
+        $locale->setCurrency($currency);
+        $this->createResource($locale);
+
+        return $locale;
+    }
+
+    public function copyLocaleData(string $sourceLocaleCode, string $targetLocaleCode)
+    {
+        $sourceLocale = $this->findLocale($sourceLocaleCode);
+        $targetLocale = $this->findLocale($targetLocaleCode);
+
+        $this->getLocaleCopier()->copyLocaleData($sourceLocale, $targetLocale);
+    }
+
+    protected function getLocaleCopier() : LocaleCopierInterface
+    {
+        return $this->get('locale.copier');
+    }
+
+    protected function findLocale(string $code) : LocaleInterface
+    {
+        return $this->repository->findOneBy(['code' => $code]);
+    }
+
+    protected function getTargetCurrency($targetCurrency) : CurrencyInterface
+    {
+        return $this->get('currency.repository')->findOneBy(['code' => $targetCurrency]);
+    }
 }
