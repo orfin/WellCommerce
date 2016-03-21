8bbe173fc5b8b075a487acedfd3244ea2851bb4a
Adam Piotrowski <adam@wellcommerce.org>
2016-03-21 02:08:39 +0100

    Composer & autoloader changes

--- web/app.php
+++ web/app.php
@@ -1,8 +1,13 @@
 <?php
 
+use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require __DIR__ . '/../app/autoload.php';
+$loader       = require __DIR__ . '/../app/autoload.php';
+$cachedLoader = new ApcClassLoader(sha1(__FILE__), $loader);
+$cachedLoader->register();
+$loader->unregister();
+
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 $request  = Request::createFromGlobals();
