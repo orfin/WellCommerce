666fda392ca571decf7badb274424f4b68b8b915
Adam Piotrowski <adam@wellcommerce.org>
2016-03-23 03:10:31 +0100

    Fixed missing files

--- Elements/Button/Button.php
+++ Elements/Button/Button.php
@@ -12,6 +12,10 @@
 
 namespace WellCommerce\Component\Form\Elements\Button;
 
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use WellCommerce\Component\Form\Elements\Attribute;
+use WellCommerce\Component\Form\Elements\AttributeCollection;
+
 /**
  * Class Button
  *
@@ -19,5 +23,36 @@ namespace WellCommerce\Component\Form\Elements\Button;
  */
 class Button extends AbstractButton implements ButtonInterface
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
 
+        $resolver->setDefaults([
+            'icon'      => '',
+            'css_class' => '',
+            'url'       => '',
+        ]);
+
+        $resolver->setAllowedTypes('icon', 'string');
+        $resolver->setAllowedTypes('url', 'string');
+        $resolver->setAllowedTypes('css_class', 'string');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function prepareAttributesCollection(AttributeCollection $collection)
+    {
+        parent::prepareAttributesCollection($collection);
+        $collection->add(new Attribute('sUrl', $this->getOption('url')));
+        $collection->add(new Attribute('sCssClass', $this->getOption('css_class')));
+    }
+
+    public function getValue()
+    {
+        return '';
+    }
 }
