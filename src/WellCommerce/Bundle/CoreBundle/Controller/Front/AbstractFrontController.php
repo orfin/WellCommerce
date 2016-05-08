9cc5c3123dc088ae731b4ba529979863801458f3
Adam Piotrowski <adam@wellcommerce.org>
2016-05-08 12:06:20 +0200

    Added breadcrumbs to front controller

---
+++ src/WellCommerce/Bundle/CoreBundle/Controller/Front/AbstractFrontController.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+namespace WellCommerce\Bundle\CoreBundle\Controller\Front;
+
+use WellCommerce\Bundle\CategoryBundle\Storage\CategoryStorageInterface;
+use WellCommerce\Bundle\ClientBundle\Entity\ClientInterface;
+use WellCommerce\Bundle\CoreBundle\Controller\AbstractController;
+use WellCommerce\Bundle\PageBundle\Storage\Front\PageStorageInterface;
+use WellCommerce\Bundle\ProducerBundle\Storage\Front\ProducerStorageInterface;
+use WellCommerce\Bundle\ProductBundle\Storage\ProductStorageInterface;
+use WellCommerce\Bundle\ProductStatusBundle\Storage\ProductStatusStorageInterface;
+use WellCommerce\Component\Breadcrumb\Provider\BreadcrumbProviderInterface;
+
+/**
+ * Class AbstractFrontController
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+abstract class AbstractFrontController extends AbstractController implements FrontControllerInterface
+{
+    protected function getBreadcrumbProvider() : BreadcrumbProviderInterface
+    {
+        return $this->get('breadcrumb.provider');
+    }
+
+    protected function getCategoryStorage() : CategoryStorageInterface
+    {
+        return $this->get('category.storage');
+    }
+
+    protected function getProductStorage() : ProductStorageInterface
+    {
+        return $this->get('product.storage');
+    }
+
+    protected function getProductStatusStorage() : ProductStatusStorageInterface
+    {
+        return $this->get('product_status.storage');
+    }
+
+    protected function getProducerStorage() : ProducerStorageInterface
+    {
+        return $this->get('producer.storage');
+    }
+    
+    protected function getPageStorage() : PageStorageInterface
+    {
+        return $this->get('page.storage');
+    }
+    
+    protected function getAuthenticatedClient() : ClientInterface
+    {
+        return $this->getSecurityHelper()->getAuthenticatedClient();
+    }
+}
