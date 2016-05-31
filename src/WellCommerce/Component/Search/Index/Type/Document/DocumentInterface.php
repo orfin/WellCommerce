2f33483b36e5bfd00e2aae6ab4886bc8c50b1787
Adam Piotrowski <adam@wellcommerce.org>
2016-05-31 22:17:22 +0200

    Fixed interfaces

---
+++ src/WellCommerce/Component/Search/Index/Type/Document/DocumentInterface.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Component\Search;
+
+use Doctrine\Common\Collections\Collection;
+
+/**
+ * Interface DocumentInterface
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+interface DocumentInterface
+{
+    public function getIdentifier() : int;
+
+    public function getFields() : Collection;
+}
