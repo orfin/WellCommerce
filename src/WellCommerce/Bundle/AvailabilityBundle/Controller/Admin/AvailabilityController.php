c15e5a1cf1900da1e03921b504e569c3d2e32696
Adam Piotrowski <adam@wellcommerce.org>
2016-07-22 00:07:42 +0200

    Added request.isXmlHttpRequest condition to routes

--- src/WellCommerce/Bundle/AvailabilityBundle/Controller/Admin/AvailabilityController.php
+++ src/WellCommerce/Bundle/AvailabilityBundle/Controller/Admin/AvailabilityController.php
@@ -12,7 +12,13 @@
 
 namespace WellCommerce\Bundle\AvailabilityBundle\Controller\Admin;
 
+use Symfony\Component\HttpFoundation\Request;
+use WellCommerce\Bundle\AvailabilityBundle\DataGrid\AvailabilityDataGrid;
+use WellCommerce\Bundle\AvailabilityBundle\Entity\Availability;
+use WellCommerce\Bundle\AvailabilityBundle\Form\Admin\AvailabilityFormBuilder;
+use WellCommerce\Bundle\AvailabilityBundle\Manager\AvailabilityManager;
 use WellCommerce\Bundle\CoreBundle\Controller\Admin\AbstractAdminController;
+use Zend\Stdlib\Response;
 
 /**
  * Class AvailabilityController
@@ -21,4 +27,81 @@ use WellCommerce\Bundle\CoreBundle\Controller\Admin\AbstractAdminController;
  */
 class AvailabilityController extends AbstractAdminController
 {
+    public function __construct(AvailabilityManager $manager, AvailabilityFormBuilder $formBuilder, AvailabilityDataGrid $dataGrid)
+    {
+        $this->manager     = $manager;
+        $this->formBuilder = $formBuilder;
+    }
+    
+    public function indexAction() : Response
+    {
+        return $this->displayTemplate('index', [
+            'datagrid' => $this->dataGrid->getInstance()
+        ]);
+    }
+    
+    public function gridAction(Request $request) : Response
+    {
+        if (!$request->isXmlHttpRequest()) {
+            return $this->getRouterHelper()->redirectToAction('index');
+        }
+        
+        try {
+            $results = $this->dataGrid->loadResults($request);
+        } catch (\Exception $e) {
+            $results = nl2br($e->getMessage());
+        }
+        
+        return $this->jsonResponse($results);
+    }
+    
+    public function addAction(Request $request) : Response
+    {
+        $resource = $this->getManager()->initResource();
+        $form     = $this->getForm($resource);
+        
+        if ($form->handleRequest()->isSubmitted()) {
+            if ($form->isValid()) {
+                $this->getManager()->createResource($resource);
+            }
+            
+            return $this->createFormDefaultJsonResponse($form);
+        }
+        
+        return $this->displayTemplate('add', [
+            'form' => $form
+        ]);
+    }
+    
+    public function editAction(Availability $availability) : Response
+    {
+        $form = $this->getForm($availability);
+        
+        if ($form->handleRequest()->isSubmitted()) {
+            if ($form->isValid()) {
+                $this->manager->save($availability);
+            }
+            
+            return $this->createFormDefaultJsonResponse($form);
+        }
+        
+        return $this->displayTemplate('edit', [
+            'form'         => $form,
+            'availability' => $availability
+        ]);
+    }
+    
+    public function deleteAction(Availability $availability) : Response
+    {
+        $this->getDoctrineHelper()->disableFilter('locale');
+        try {
+            $resource = $this->getManager()->getRepository()->findOneBy(['id' => $id]);
+            $this->getManager()->removeResource($resource);
+        } catch (\Exception $e) {
+            return $this->jsonResponse(['error' => $e->getTraceAsString()]);
+        }
+        
+        return $this->jsonResponse(['success' => true]);
+    }
+    
 }
