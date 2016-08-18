fc3126686f024d9d97aa9c7194ae410f58364495
Adam Piotrowski <adam@wellcommerce.org>
2016-08-18 13:46:54 +0200

    Changed forms escaping

--- src/WellCommerce/Bundle/CoreBundle/Twig/Extension/FormJavascriptExtension.php
+++ src/WellCommerce/Bundle/CoreBundle/Twig/Extension/FormJavascriptExtension.php
@@ -25,12 +25,12 @@ final class FormJavascriptExtension extends \Twig_Extension
      * @var FormRendererInterface
      */
     private $renderer;
-
+    
     /**
      * @var \Twig_Environment
      */
     private $environment;
-
+    
     /**
      * FormJavascriptExtension constructor.
      *
@@ -40,31 +40,36 @@ final class FormJavascriptExtension extends \Twig_Extension
     {
         $this->renderer = $renderer;
     }
-
+    
     public function initRuntime(\Twig_Environment $environment)
     {
         $this->environment = $environment;
     }
-
+    
     public function getFunctions()
     {
         return [
-            new \Twig_SimpleFunction('form_js', [$this, 'render'], ['is_safe' => ['html', 'javascript']])
+            new \Twig_SimpleFunction('form_js', [$this, 'render'], ['is_safe' => ['html', 'javascript']]),
+            new \Twig_SimpleFunction('form_javascript', [$this, 'renderJavascript'], ['is_safe' => ['html', 'javascript']])
         ];
     }
-
+    
     public function getName()
     {
         return 'form_js';
     }
-
+    
     public function render(FormInterface $form) : string
     {
         $templateVars = [
-            'form'            => $form,
-            'form_javascript' => $this->renderer->renderForm($form),
+            'form' => $form
         ];
-
+        
         return $this->environment->render($this->renderer->getTemplateName(), $templateVars);
+    }
+    
+    public function renderJavascript(FormInterface $form) : string
+    {
+        return $this->renderer->renderForm($form);
     }
 }
