954856572b0b901bc599ab5dfb278ce9f2cc7424
Adam Piotrowski <adam@wellcommerce.org>
2016-08-20 11:25:38 +0200

    Initial WishlistBundle structure

---
+++ src/WellCommerce/Bundle/WishlistBundle/WellCommerceWishlistBundle.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\WishlistBundle;
+
+use WellCommerce\Bundle\CoreBundle\HttpKernel\AbstractWellCommerceBundle;
+
+/**
+ * Class WellCommerceWishlistBundle
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+final class WellCommerceWishlistBundle extends AbstractWellCommerceBundle
+{
+}
