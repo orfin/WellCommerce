cfe70e52648bf6645fde4fad4f87cf95014fbb95
Adam Piotrowski <adam@wellcommerce.org>
2016-11-03 01:48:11 +0100

    Added ability to set field options in D2 enhancers

--- src/WellCommerce/Bundle/CoreBundle/Definition/FieldDefinition.php
+++ src/WellCommerce/Bundle/CoreBundle/Definition/FieldDefinition.php
@@ -24,7 +24,7 @@ class FieldDefinition extends AbstractMappingDefinition
     /**
      * {@inheritdoc}
      */
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions (OptionsResolver $resolver)
     {
         $resolver->setRequired([
             'fieldName',
@@ -33,26 +33,29 @@ class FieldDefinition extends AbstractMappingDefinition
             'unique',
             'nullable',
             'columnName',
+            'options',
         ]);
-
+        
         $resolver->setDefaults([
             'length'   => 50,
             'unique'   => false,
             'nullable' => true,
+            'options'  => [],
         ]);
-
+        
         $resolver->setAllowedTypes('fieldName', 'string');
         $resolver->setAllowedTypes('type', 'string');
         $resolver->setAllowedTypes('length', 'integer');
         $resolver->setAllowedTypes('unique', 'boolean');
         $resolver->setAllowedTypes('nullable', 'boolean');
         $resolver->setAllowedTypes('columnName', 'string');
+        $resolver->setAllowedTypes('options', 'array');
     }
-
+    
     /**
      * {@inheritdoc}
      */
-    public function getClassMetadataMethod()
+    public function getClassMetadataMethod ()
     {
         return MappingDefinitionInterface::CLASS_METADATA_METHOD_FIELD;
     }
